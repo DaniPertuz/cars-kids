6f9c1b6f6b05f87809a78a3c4a994244b7598e36
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-13 23:48:26 -0500

    Configure carskidsApi

---
+++ src/config/api/carskidsApi.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { API_URL as PROD_URL, API_URL_ANDROID, API_URL_IOS, STAGE } from '@env';
+import { Platform } from 'react-native';
+
+export const API_URL =
+  (STAGE === 'prod')
+    ? PROD_URL
+    : Platform.OS === 'ios'
+      ? API_URL_IOS
+      : API_URL_ANDROID;
+
+const carskidsApi = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+export default carskidsApi;
