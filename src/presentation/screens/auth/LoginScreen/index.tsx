f0c0cb122989b0aa4c631343a1387e577874708f
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-13 21:29:34 -0500

    Add navigation and update styles

--- src/presentation/screens/auth/LoginScreen/index.tsx
+++ src/presentation/screens/auth/LoginScreen/index.tsx
@@ -1,32 +1,36 @@
 import { Image, ScrollView, useWindowDimensions } from 'react-native';
+import { StackScreenProps } from '@react-navigation/stack';
 import { Layout, Text } from '@ui-kitten/components';
 
 import { EmailInput, PasswordInput } from '../../../components/forms';
 import { PrimaryButton, Spacer } from '../../../components/ui';
 import { Footer } from '../../../components/Login/footer';
+import { RootStackParams } from '../../../navigation/MainNavigator';
 
+import { authStyles } from '../../../styles/auth/styles';
 import { globalStyles } from '../../../styles/global.styles';
-import { styles } from './styles';
 
-export const LoginScreen = () => {
+interface Props extends StackScreenProps<RootStackParams, 'LoginScreen'> {}
+
+export const LoginScreen = ({ navigation }: Props) => {
   const { height } = useWindowDimensions();
 
   return (
-    <Layout style={styles.container}>
-      <ScrollView style={styles.mainMargin}>
-        <Layout style={{ paddingTop: height * 0.1, ...styles.mainLayout }}>
-          <Layout style={styles.imageContainer}>
+    <Layout style={authStyles.container}>
+      <ScrollView style={authStyles.mainMargin}>
+        <Layout style={{ paddingTop: height * 0.1, ...authStyles.mainLayout }}>
+          <Layout style={authStyles.imageContainer}>
             <Image
               source={require('../../../../assets/carkids-removebg.png')}
-              style={styles.mainImage}
+              style={authStyles.mainImage}
             />
           </Layout>
-          <Layout style={styles.welcomeTextContainer}>
-            <Text category='h1' style={styles.primaryText}>Bienvenido</Text>
+          <Layout style={authStyles.welcomeTextContainer}>
+            <Text category='h1' style={globalStyles.colorOnyx}>Bienvenido</Text>
           </Layout>
-          <Text category='p1' style={styles.secondaryText}>Ingresa tus credenciales</Text>
+          <Text category='p1' style={globalStyles.colorSpanishGray}>Ingresa tus credenciales</Text>
         </Layout>
-        <Layout style={styles.formContainer}>
+        <Layout style={authStyles.formContainer}>
           <EmailInput />
           <PasswordInput />
         </Layout>
@@ -35,7 +39,7 @@ export const LoginScreen = () => {
           <PrimaryButton text='Ingresar' onPress={() => { }} />
         </Layout>
         <Spacer height={50} />
-        <Footer text='¿No tienes cuenta?' linkText='Crea una' onPress={() => { }} />
+        <Footer text='¿No tienes cuenta?' linkText='Crea una' onPress={() => { navigation.push('RegisterScreen') }} />
       </ScrollView>
     </Layout>
   );
