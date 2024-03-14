daafad56eb63f499aa61efaf48bac3a1d473e9c7
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-14 16:52:55 -0500

    Add storage adapter and logout function

--- src/presentation/store/auth/useAuthStore.ts
+++ src/presentation/store/auth/useAuthStore.ts
@@ -1,12 +1,14 @@
 import { create } from 'zustand';
-import { AuthStatus, IUser } from '../../../infrastructure/interfaces';
 import { authLogin } from '../../../actions/auth/auth';
+import { AuthStatus, IUser } from '../../../infrastructure/interfaces';
+import { StorageAdapter } from '../../../config/adapters/storage-adapter';
 
 export interface AuthState {
   status: AuthStatus;
   token?: string;
   user?: IUser;
   login: (email: string, password: string) => Promise<boolean>;
+  logout: () => Promise<void>;
 }
 
 export const useAuthStore = create<AuthState>()((set, get) => ({
@@ -16,12 +18,19 @@ export const useAuthStore = create<AuthState>()((set, get) => ({
 
   login: async (email: string, password: string) => {
     const resp = await authLogin(email, password);
+
     if (!resp) {
       set({ status: 'unauthenticated', token: undefined, user: undefined });
       return false;
     }
 
+    await StorageAdapter.setItem('token', resp.token);
+
     set({ status: 'authenticated', token: resp.token, user: resp.user });
     return true;
+  },
+  logout: async () => {
+    await StorageAdapter.removeItem('token');
+    set({ status: 'unauthenticated', token: undefined, user: undefined });
   }
 }));
