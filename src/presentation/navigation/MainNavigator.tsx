cfad89d6f83717d2a74169a9725a85d184ee2ee6
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-13 23:25:04 -0500

    Implement StatusBarComponent on MainNavigator

--- src/presentation/navigation/MainNavigator.tsx
+++ src/presentation/navigation/MainNavigator.tsx
@@ -1,5 +1,6 @@
 import { StackCardStyleInterpolator, createStackNavigator } from '@react-navigation/stack';
 
+import StatusBarComponent from '../components/ui/status-bar';
 import { LoadingScreen } from '../screens/LoadingScreen';
 import { HomeScreen } from '../screens/home/HomeScreen';
 import { LoginScreen } from '../screens/auth/LoginScreen';
@@ -24,16 +25,19 @@ const fadeAnimation: StackCardStyleInterpolator = ({ current }) => {
 
 export const MainNavigator = () => {
   return (
-    <Stack.Navigator
-      initialRouteName='LoginScreen'
-      screenOptions={{
-        headerShown: false
-      }}
-    >
-      <Stack.Screen options={{ cardStyleInterpolator: fadeAnimation }} name="LoadingScreen" component={LoadingScreen} />
-      <Stack.Screen options={{ cardStyleInterpolator: fadeAnimation }} name="LoginScreen" component={LoginScreen} />
-      <Stack.Screen options={{ cardStyleInterpolator: fadeAnimation }} name="RegisterScreen" component={RegisterScreen} />
-      <Stack.Screen options={{ cardStyleInterpolator: fadeAnimation }} name="HomeScreen" component={HomeScreen} />
-    </Stack.Navigator>
+    <>
+      <StatusBarComponent color={'#DCD7D7'} theme='dark-content' />
+      <Stack.Navigator
+        initialRouteName='LoginScreen'
+        screenOptions={{
+          headerShown: false
+        }}
+      >
+        <Stack.Screen options={{ cardStyleInterpolator: fadeAnimation }} name="LoadingScreen" component={LoadingScreen} />
+        <Stack.Screen options={{ cardStyleInterpolator: fadeAnimation }} name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen options={{ cardStyleInterpolator: fadeAnimation }} name="RegisterScreen" component={RegisterScreen} />
+        <Stack.Screen options={{ cardStyleInterpolator: fadeAnimation }} name="HomeScreen" component={HomeScreen} />
+      </Stack.Navigator>
+    </>
   );
 };
