f7024fdb46d5219526a9d225de72ecba569decf3
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-13 21:18:24 -0500

    Styles for auth section screens

---
+++ src/presentation/styles/auth/styles.ts
@@ -0,0 +1,44 @@
+import { StyleSheet } from 'react-native';
+import { globalStyles } from '../global.styles';
+
+export const authStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    ...globalStyles.mainBackground
+  },
+  formContainer: {
+    ...globalStyles.mainBackground,
+    marginTop: 20,
+  },
+  footer: {
+    alignItems: 'flex-end',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    ...globalStyles.mainBackground
+  },
+  imageContainer: {
+    alignItems: 'center',
+    ...globalStyles.mainBackground,
+    marginBottom: 30
+  },
+  mainImage: {
+    ...globalStyles.mainBackground,
+    height: 200,
+    width: 300
+  },
+  mainLayout: {
+    ...globalStyles.mainBackground,
+    gap: 20,
+  },
+  mainMargin: {
+    marginHorizontal: 40
+  },
+  spacer: {
+    ...globalStyles.mainBackground,
+    height: 20
+  },
+  welcomeTextContainer: {
+    alignItems: 'center',
+    ...globalStyles.mainBackground
+  }
+});
