74ab55efa1dec1873aa424ec08c45f781f786771
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-13 20:09:33 -0500

    Footer component for Login screen

---
+++ src/presentation/components/Login/footer/index.tsx
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+import { globalStyles } from '../../../styles/global.styles';
+import { Layout, Text } from '@ui-kitten/components';
+
+interface Props {
+  text: string;
+  linkText: string;
+  onPress: () => void;
+}
+
+export const Footer = ({ text, linkText, onPress }: Props) => {
+  return (
+    <Layout style={styles.footer}>
+      <Text>{text} </Text>
+      <Text style={globalStyles.colorPrimaryRed} onPress={onPress}>
+        {linkText}
+      </Text>
+    </Layout>
+  );
+};
+
+const styles = StyleSheet.create({
+  footer: {
+    alignItems: 'flex-end',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    ...globalStyles.mainBackground
+  }
+});
