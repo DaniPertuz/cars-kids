03f02c712e4885e2e010d4842501e0090fdc9b0f
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-13 19:52:23 -0500

    Global styles

---
+++ src/presentation/styles/global.styles.ts
@@ -0,0 +1,39 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  input: {
+    borderColor: '#DCD7D7',
+    borderRadius: 10,
+    marginBottom: 10
+  },
+  mainBackground: {
+    backgroundColor: '#DCD7D7'
+  },
+  colorPrimaryRed: {
+    color: '#E92E29'
+  },
+  colorSecondaryRed: {
+    color: '#D1544C'
+  },
+  colorTerciaryRed: {
+    color: '#D46364'
+  },
+  colorOnyx: {
+    color: '#33393C'
+  },
+  colorDeepTaupe: {
+    color: '#7C6466'
+  },
+  colorLightSilver: {
+    color: '#DCD7D7'
+  },
+  colorOrangeCrayola: {
+    color: '#F47E36'
+  },
+  colorSpanishGray: {
+    color: '#9A989E'
+  },
+  colorWintergreenDream: {
+    color: '#5B8484'
+  }
+});
