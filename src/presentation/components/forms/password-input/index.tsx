d8476bf57672186717d33a0ddebd58a840ae3a18
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-15 19:36:23 -0500

    Handle password visibility

--- src/presentation/components/forms/password-input/index.tsx
+++ src/presentation/components/forms/password-input/index.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { Input, useTheme } from '@ui-kitten/components';
+import { DisplayPassword } from '../DisplayPassword';
 import { globalStyles } from '../../../styles/global.styles';
 
 interface Props {
@@ -10,14 +12,21 @@ interface Props {
 export const PasswordInput = ({ placeholder, value, onChangeText }: Props) => {
   const theme = useTheme();
 
+  const [isPasswordVisible, setIsPasswordVisible] = useState(true);
+
+  const handlePasswordVisibility = (isVisible: boolean) => {
+    setIsPasswordVisible(isVisible);
+  };
+
   return (
     <Input
       placeholder={placeholder}
       autoCapitalize='none'
-      secureTextEntry
+      secureTextEntry={isPasswordVisible}
       value={value}
       onChangeText={onChangeText}
       textStyle={globalStyles.colorOnyx}
+      accessoryRight={<DisplayPassword handlePasswordVisibility={handlePasswordVisibility} />}
       style={[globalStyles.input, { backgroundColor: theme['color-basic-100'] }]}
     />
   );
