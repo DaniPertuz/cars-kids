b30d7bcdc23478ba8d6016c6cb49095c0b5593c2
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-15 19:34:35 -0500

    EyeClosedIcon UI component

---
+++ src/presentation/components/ui/EyeIconClosed/index.tsx
@@ -0,0 +1,8 @@
+import { Icon, IconElement } from '@ui-kitten/components';
+
+export const EyeClosedIcon = (props: any): IconElement => (
+  <Icon
+    {...props}
+    name='eye-off-outline'
+  />
+);
