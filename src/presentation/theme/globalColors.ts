0508e764217a2d8e538e1f4da53fdab7c81254ed
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-18 23:28:13 -0500

    Define app global colors

---
+++ src/presentation/theme/globalColors.ts
@@ -0,0 +1,12 @@
+export const globalColors = {
+  background: '#DCD7D7',
+  backgroundDark: '#9A989E',
+  dark: '#33393C',
+  darkLight: '#7C6466',
+  primaryRed: '#E92E29',
+  secondaryRed: '#D1544C',
+  tertiaryRed: '#D46364',
+  success: '#5B8484',
+  warning: '#F47E36',
+  white: '#FFFFFF'
+};
