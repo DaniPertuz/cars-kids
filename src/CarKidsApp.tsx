cf516f3667867fac9a7477df0551cf401d21c7f8
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-18 23:46:10 -0500

    Update theme colors set

--- src/CarKidsApp.tsx
+++ src/CarKidsApp.tsx
@@ -4,7 +4,7 @@ import { useColorScheme } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import { ApplicationProvider, IconRegistry, useTheme } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
 import { MainNavigator } from './presentation/navigation/MainNavigator';
@@ -12,11 +12,12 @@ import { AuthProvider } from './presentation/providers/AuthProvider';
 
 export const CarKidsApp = () => {
   const colorScheme = useColorScheme();
+  const uTheme = useTheme();
   const theme = colorScheme === 'dark' ? eva.dark : eva.light;
   const backgroundColor =
     colorScheme === 'dark'
-      ? theme['color-basic-800']
-      : theme['color-basic-100'];
+      ? uTheme['color-basic-800']
+      : uTheme['color-basic-100'];
 
   return (
     <>
@@ -25,12 +26,12 @@ export const CarKidsApp = () => {
         <NavigationContainer theme={{
           dark: colorScheme === 'dark',
           colors: {
-            primary: theme['color-primary-500'],
+            primary: uTheme['color-primary-500'],
             background: backgroundColor,
-            card: theme['color-basic-100'],
-            text: theme['text-basic-color'],
-            border: theme['color-basic-800'],
-            notification: theme['color-primary-500']
+            card: uTheme['color-basic-100'],
+            text: uTheme['text-basic-color'],
+            border: uTheme['color-basic-800'],
+            notification: uTheme['color-primary-500']
           }
         }}>
           <AuthProvider>
