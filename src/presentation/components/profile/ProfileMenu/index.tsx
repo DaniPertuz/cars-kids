ce194eb1ec1bdf1db992aa78f581b7de13f20b62
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-23 12:06:48 -0500

    Add onPress prop to apply navigation

--- src/presentation/components/profile/ProfileMenu/index.tsx
+++ src/presentation/components/profile/ProfileMenu/index.tsx
@@ -1,16 +1,23 @@
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
 import { Layout } from '@ui-kitten/components';
+
+import { RootStackParams } from '../../../navigation/MainNavigator';
 import { ProfileMenuItem } from '../ProfileMenuItem';
 import { LogoutComponent } from '../Logout';
+
 import { styles } from './styles';
 
 export const ProfileMenu = () => {
+  const navigation = useNavigation<StackNavigationProp<RootStackParams>>();
+
   return (
     <Layout style={styles.menuContainer}>
       <Layout style={styles.menuItemsContainer}>
-        <ProfileMenuItem iconName={'edit-outline'} label={'Actualizar datos'} />
-        <ProfileMenuItem iconName={'car-outline'} label={'Vehículos'} />
-        <ProfileMenuItem iconName={'credit-card-outline'} label={'Presupuesto'} />
-        <ProfileMenuItem iconName={'archive-outline'} label={'Productos'} />
+        <ProfileMenuItem iconName={'edit-outline'} label={'Actualizar datos'} onPress={() => navigation.navigate('UpdateProfileScreen')} />
+        <ProfileMenuItem iconName={'car-outline'} label={'Vehículos'} onPress={() => navigation.navigate('BottomNavigator')} />
+        <ProfileMenuItem iconName={'credit-card-outline'} label={'Presupuesto'} onPress={() => navigation.navigate('BottomNavigator')} />
+        <ProfileMenuItem iconName={'archive-outline'} label={'Productos'} onPress={() => navigation.navigate('BottomNavigator')} />
       </Layout>
       <LogoutComponent />
     </Layout>
--- src/presentation/components/profile/ProfileMenuItem/index.tsx
+++ src/presentation/components/profile/ProfileMenuItem/index.tsx
@@ -1,3 +1,4 @@
+import { TouchableOpacity } from 'react-native';
 import { Layout, Text } from '@ui-kitten/components';
 import { CustomIcon } from '../../ui';
 import { styles } from './styles';
@@ -5,16 +6,19 @@ import { styles } from './styles';
 interface Props {
   iconName: string;
   label: string;
+  onPress: () => void;
 }
 
-export const ProfileMenuItem = ({ iconName, label }: Props) => {
+export const ProfileMenuItem = ({ iconName, label, onPress }: Props) => {
   return (
     <Layout style={styles.menuItem}>
       <Layout style={styles.menuItemLabel}>
         <CustomIcon name={iconName} />
         <Text category='h6'>{label}</Text>
       </Layout>
-      <CustomIcon name='arrow-circle-right-outline' />
+      <TouchableOpacity activeOpacity={1} onPress={onPress}>
+        <CustomIcon name='arrow-circle-right-outline' />
+      </TouchableOpacity>
     </Layout>
   );
 };
