287227fffa469ea28c45a734ba5dd6d2a1356fc2
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-23 13:18:25 -0500

    Add logout action

--- src/presentation/components/profile/Logout/index.tsx
+++ src/presentation/components/profile/Logout/index.tsx
@@ -1,14 +1,22 @@
+import { TouchableOpacity } from 'react-native';
 import { Layout, Text } from '@ui-kitten/components';
+
 import { CustomIcon } from '../../ui';
+import { useAuthStore } from '../../../store/auth/useAuthStore';
+
 import { globalColors } from '../../../theme/globalColors';
 import { globalStyles } from '../../../styles/global.styles';
 import { styles } from './styles';
 
 export const LogoutComponent = () => {
+  const { logout } = useAuthStore();
+
   return (
-    <Layout style={styles.logoutContainer}>
-      <CustomIcon name='power-outline' fillColor={globalColors.primaryRed} />
-      <Text category='h6' style={globalStyles.colorPrimaryRed}>Cerrar sesión</Text>
-    </Layout>
+    <TouchableOpacity activeOpacity={1.0} onPress={logout}>
+      <Layout style={styles.logoutContainer}>
+        <CustomIcon name='power-outline' fillColor={globalColors.primaryRed} />
+        <Text category='h6' style={globalStyles.colorPrimaryRed}>Cerrar sesión</Text>
+      </Layout>
+    </TouchableOpacity>
   );
 };
