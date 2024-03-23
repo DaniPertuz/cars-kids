3c416e281dee29c82bea2513a83fd9c935e23fcd
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-22 22:03:13 -0500

    Add prop to customize icon size

--- src/presentation/components/ui/CustomIcon/index.tsx
+++ src/presentation/components/ui/CustomIcon/index.tsx
@@ -4,6 +4,7 @@ import { globalStyles } from '../../../styles/global.styles';
 interface Props {
   name: string;
   fillColor?: string;
+  size?: { height: number, width: number };
 }
 
-export const CustomIcon = ({ name, fillColor }: Props): IconElement => <Icon style={globalStyles.iconSize} name={name} fill={fillColor} />;
+export const CustomIcon = ({ name, fillColor, size }: Props): IconElement => <Icon style={!size ? globalStyles.iconSize : size} name={name} fill={fillColor} />;
