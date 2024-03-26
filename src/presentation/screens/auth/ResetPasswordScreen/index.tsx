54ec545e2790ee8e43609333e9a0445e9e3c15b4
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-26 12:52:33 -0500

    Apply useResetPassword custom hook

--- src/presentation/screens/auth/ResetPasswordScreen/index.tsx
+++ src/presentation/screens/auth/ResetPasswordScreen/index.tsx
@@ -1,60 +1,19 @@
-import { useState } from 'react';
 import { ScrollView, useWindowDimensions } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { StackScreenProps } from '@react-navigation/stack';
 import { Layout } from '@ui-kitten/components';
-import Snackbar from 'react-native-snackbar';
 
 import { EmailInput, PasswordInput } from '../../../components/forms';
 import { LoginButtonContainer, LoginMainImage } from '../../../components/login';
 import { BackButtonContainer, Footnote, Headline } from '../../../components/ui';
-import { useEmptyFieldValidation } from '../../../hooks/useEmptyFieldValidation';
-import { RootStackParams } from '../../../navigation/MainNavigator';
-import { useAuthStore } from '../../../store/auth/useAuthStore';
+import { useResetPassword } from '../../../hooks';
 
 import { authStyles } from '../../../styles/auth/styles';
 import { globalStyles } from '../../../styles/global.styles';
 
-interface Props extends StackScreenProps<RootStackParams, 'ResetPasswordScreen'> { }
-
-export const ResetPasswordScreen = ({ navigation }: Props) => {
+export const ResetPasswordScreen = () => {
   const { top } = useSafeAreaInsets();
   const { height } = useWindowDimensions();
-  const { updatePassword } = useAuthStore();
-  const [form, setForm] = useState({
-    email: '',
-    password: '',
-    confirmPassword: ''
-  });
-
-  const { email, password, confirmPassword } = form;
-
-  const { isEmpty: isEmailEmpty, checkEmptyFields: checkEmailEmpty } = useEmptyFieldValidation();
-  const { isEmpty: isPasswordEmpty, checkEmptyFields: checkPasswordEmpty } = useEmptyFieldValidation();
-  const { isEmpty: isConfirmedPasswordEmpty, checkEmptyFields: checkConfirmedPasswordEmpty } = useEmptyFieldValidation();
-
-  const onUpdatePassword = async () => {
-    checkEmailEmpty(email);
-    checkPasswordEmpty(password);
-    checkConfirmedPasswordEmpty(confirmPassword);
-
-    if (!isEmailEmpty && !isPasswordEmpty && !isConfirmedPasswordEmpty) {
-      if (password !== confirmPassword) {
-        Snackbar.show({ text: 'Contraseñas no coinciden', duration: Snackbar.LENGTH_SHORT });
-        return;
-      }
-
-      const resp = await updatePassword(email, password);
-
-      if (resp.error) {
-        Snackbar.show({ text: resp.error, duration: Snackbar.LENGTH_SHORT });
-        return;
-      }
-
-      Snackbar.show({ text: 'Contraseña restablecida', duration: Snackbar.LENGTH_SHORT });
-      navigation.replace('LoginScreen');
-    }
-  };
+  const { form, email, password, confirmPassword, setForm, onUpdatePassword } = useResetPassword();
 
   return (
     <Layout style={globalStyles.container}>
