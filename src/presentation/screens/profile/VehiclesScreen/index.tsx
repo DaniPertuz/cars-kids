0c98f89e8b935f2587cf62f7a17ce0ec0fc430e1
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-27 16:58:49 -0500

    VehiclesScreen

---
+++ src/presentation/screens/profile/VehiclesScreen/index.tsx
@@ -0,0 +1,30 @@
+import { useWindowDimensions } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Layout } from '@ui-kitten/components';
+
+import { BackButtonContainer } from '../../../components/ui';
+import { VehicleListComponent } from '../../../components/vehicles/VehiclesListComponent';
+import { VehiclesListPagination } from '../../../components/vehicles/VehiclesListPagination';
+import { VehicleAddButton } from '../../../components/vehicles/VehicleAddButton';
+import { VehicleTitleHeader } from '../../../components/vehicles/VehicleTitleHeader';
+import { useVehiclesData } from '../../../hooks';
+
+import { globalStyles } from '../../../styles/global.styles';
+
+export const VehiclesScreen = () => {
+  const { top, bottom } = useSafeAreaInsets();
+  const { height } = useWindowDimensions();
+  const { display, vehiclesData } = useVehiclesData();
+
+  return (
+    <Layout style={globalStyles.container}>
+      <Layout style={{ paddingTop: height * 0.042, ...globalStyles.mainLayout }}>
+        <BackButtonContainer top={top} />
+        <VehicleTitleHeader />
+        <VehicleListComponent bottom={bottom} display={display} vehiclesData={vehiclesData} />
+      </Layout>
+      <VehiclesListPagination bottom={bottom} vehiclesData={vehiclesData} />
+      <VehicleAddButton top={top} />
+    </Layout>
+  );
+};
