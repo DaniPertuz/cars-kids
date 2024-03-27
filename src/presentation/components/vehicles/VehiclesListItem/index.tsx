a14cf871810936c265e1d6366bd3ef66f7ef1815
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-27 16:51:12 -0500

    VehiclesListItem Vehicle component and styles

---
+++ src/presentation/components/vehicles/VehiclesListItem/index.tsx
@@ -0,0 +1,26 @@
+import { ListItem } from '@ui-kitten/components';
+
+import { VehicleActions } from '../VehicleActions';
+import { VehicleDescription } from '../VehicleDescription';
+import { VehicleImage } from '../VehicleImage';
+import { VehicleTitle } from '../VehicleTitle';
+
+import { IVehicle } from '../../../../infrastructure/interfaces';
+
+import { styles } from './styles';
+
+interface Props {
+  item: IVehicle;
+}
+
+export const VehiclesListItem = ({ item }: Props) => {
+  return (
+    <ListItem
+      style={styles.container}
+      title={<VehicleTitle item={item} />}
+      description={<VehicleDescription item={item} />}
+      accessoryLeft={<VehicleImage />}
+      accessoryRight={<VehicleActions />}
+    />
+  );
+};
---
+++ src/presentation/components/vehicles/VehiclesListItem/styles.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+import { globalColors } from '../../../theme/globalColors';
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: globalColors.platinum,
+    borderRadius: 10,
+    gap: 10,
+    margin: 10,
+    paddingHorizontal: 10
+  },
+  itemBackground: {
+    backgroundColor: globalColors.platinum
+  }
+});
