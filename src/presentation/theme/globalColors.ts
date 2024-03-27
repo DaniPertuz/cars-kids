30d8d0f27384c3ccbd9220e89197e81d1496af75
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-27 16:38:11 -0500

    Add new global color

--- src/presentation/theme/globalColors.ts
+++ src/presentation/theme/globalColors.ts
@@ -6,6 +6,7 @@ export const globalColors = {
   primaryRed: '#E92E29',
   secondaryRed: '#D1544C',
   tertiaryRed: '#D46364',
+  platinum: '#E7E7E7',
   success: '#5B8484',
   warning: '#F47E36',
   white: '#FFFFFF'
