6d82bf4fdb6f0dc56706ee7172ca00556064fef6
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-28 12:22:58 -0500

    Set dynamic url param

--- src/actions/vehicles/index.ts
+++ src/actions/vehicles/index.ts
@@ -2,9 +2,9 @@ import { AxiosError } from 'axios';
 import carskidsApi from '../../config/api/carskidsApi';
 import { VehiclesResponse } from '../../infrastructure/interfaces';
 
-export const getVehicles = async () => {
+export const getVehicles = async (url: string) => {
   try {
-    const { data } = await carskidsApi.get<VehiclesResponse>('vehicles');
+    const { data } = await carskidsApi.get<VehiclesResponse>(url);
 
     return data;
   } catch (error: any) {
