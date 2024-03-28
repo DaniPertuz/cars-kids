faf5065a2f1501324d8a1abc7893f30370c69a01
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-28 12:30:45 -0500

    Add url param to set button performance

--- src/presentation/components/ui/NextButton/index.tsx
+++ src/presentation/components/ui/NextButton/index.tsx
@@ -1,19 +1,22 @@
 import { TouchableOpacity } from 'react-native';
 import { Icon } from '@ui-kitten/components';
+import { globalColors } from '../../../theme/globalColors';
 
 interface Props {
   iconSize: number;
+  nextUrl: string | null;
   onPress: () => void;
 }
 
-export const NextButton = ({ iconSize, onPress }: Props) => {
+export const NextButton = ({ iconSize, nextUrl, onPress }: Props) => {
   return (
     <TouchableOpacity
       activeOpacity={1.0}
+      disabled={!nextUrl}
       onPress={onPress}
       style={{ height: iconSize, width: iconSize }}
     >
-      <Icon name='arrow-circle-right-outline' />
+      <Icon name='arrow-circle-right-outline' fill={nextUrl ? globalColors.dark : globalColors.darkDisabled} />
     </TouchableOpacity>
   );
 };
--- src/presentation/components/ui/PrevButton/index.tsx
+++ src/presentation/components/ui/PrevButton/index.tsx
@@ -1,19 +1,22 @@
 import { Icon } from '@ui-kitten/components';
 import { TouchableOpacity } from 'react-native';
+import { globalColors } from '../../../theme/globalColors';
 
 interface Props {
   iconSize: number;
+  prevUrl: string | null;
   onPress: () => void;
 }
 
-export const PrevButton = ({ iconSize, onPress }: Props) => {
+export const PrevButton = ({ iconSize, prevUrl, onPress }: Props) => {
   return (
     <TouchableOpacity
       activeOpacity={1.0}
       onPress={onPress}
+      disabled={!prevUrl}
       style={{ height: iconSize, width: iconSize }}
     >
-      <Icon name='arrow-circle-left-outline' />
+      <Icon name='arrow-circle-left-outline' fill={prevUrl ? globalColors.dark : globalColors.darkDisabled} />
     </TouchableOpacity>
   );
 };
