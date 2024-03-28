e328c9c3c9191651ebbc6555042c09f8e026a3d7
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-28 12:34:15 -0500

    Update conditional rendering

--- src/presentation/components/vehicles/VehiclesListComponent/index.tsx
+++ src/presentation/components/vehicles/VehiclesListComponent/index.tsx
@@ -1,8 +1,10 @@
-import { Layout } from '@ui-kitten/components';
+import { Layout, Text } from '@ui-kitten/components';
 import { VehiclesList } from '../VehiclesList';
 import { VehiclesResponse } from '../../../../infrastructure/interfaces';
 import { LoadingScreen } from '../../../screens/LoadingScreen';
 import { globalStyles } from '../../../styles/global.styles';
+import { HeaderFive } from '../../ui';
+import { EmptyVehiclesListMessage } from '../EmptyVehiclesListMessage';
 
 interface Props {
   bottom: number;
@@ -13,11 +15,15 @@ interface Props {
 export const VehicleListComponent = ({ bottom, display, vehiclesData }: Props) => {
   return (
     <Layout style={{ ...globalStyles.mainBackground, marginHorizontal: 20, paddingBottom: bottom + 150 }}>
-      {!display
+      {(!display)
         ?
         <LoadingScreen />
         :
-        <VehiclesList vehiclesData={vehiclesData} />
+        (vehiclesData.total === 0)
+          ?
+          <EmptyVehiclesListMessage />
+          :
+          <VehiclesList vehiclesData={vehiclesData} />
       }
     </Layout>
   );
