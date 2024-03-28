c9c9914ec84aa8512f9a1f9c03e7b12e6f50bef9
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-28 12:32:37 -0500

    Set functions to perform pagination

--- src/presentation/components/vehicles/VehiclesListPagination/index.tsx
+++ src/presentation/components/vehicles/VehiclesListPagination/index.tsx
@@ -1,4 +1,5 @@
-import { Layout, Text } from '@ui-kitten/components';
+import { useEffect, useState } from 'react';
+import { Layout } from '@ui-kitten/components';
 import { VehiclesResponse } from '../../../../infrastructure/interfaces';
 import { HeaderFive, NextButton, PrevButton } from '../../ui';
 import { styles } from './styles';
@@ -6,14 +7,36 @@ import { styles } from './styles';
 interface Props {
   bottom: number;
   vehiclesData: VehiclesResponse;
+  fetchNextPage: (url: string) => void;
+  fetchPrevPage: (url: string) => void;
 }
 
-export const VehiclesListPagination = ({ bottom, vehiclesData }: Props) => {
+export const VehiclesListPagination = ({ bottom, vehiclesData, fetchPrevPage, fetchNextPage }: Props) => {
+  const [prevUrl, setPrevUrl] = useState<string | null>(null);
+  const [nextUrl, setNextUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    setPrevUrl(vehiclesData.prev);
+    setNextUrl(vehiclesData.next);
+  }, [vehiclesData.prev, vehiclesData.next]);
+
+  const handlePrevClick = () => {
+    if (prevUrl) {
+      fetchPrevPage(prevUrl);
+    }
+  };
+
+  const handleNextClick = () => {
+    if (nextUrl) {
+      fetchNextPage(nextUrl);
+    }
+  };
+
   return (
     <Layout style={{ ...styles.container, bottom: bottom + 20 }}>
-      <PrevButton iconSize={30} onPress={() => { }} />
+      <PrevButton iconSize={30} prevUrl={prevUrl} onPress={handlePrevClick} />
       <HeaderFive text={String(vehiclesData.page)} />
-      <NextButton iconSize={30} onPress={() => { }} />
+      <NextButton iconSize={30} nextUrl={nextUrl} onPress={handleNextClick} />
     </Layout>
   );
 };
