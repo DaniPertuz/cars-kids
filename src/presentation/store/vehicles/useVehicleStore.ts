d5dc30c1d2b6efbba5cdd50d25fd2618911b3cec
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-28 12:28:56 -0500

    Define dynamic url param and state

--- src/presentation/store/vehicles/useVehicleStore.ts
+++ src/presentation/store/vehicles/useVehicleStore.ts
@@ -4,13 +4,14 @@ import { getVehicles } from '../../../actions/vehicles';
 
 export interface VehicleState {
   vehicles: IVehicle[];
-  getVehicles: () => Promise<any>;
+  getVehicles: (url: string) => Promise<any>;
 }
 
 export const useVehicleStore = create<VehicleState>()((set, get) => ({
   vehicles: [],
-  getVehicles: async () => {
-    const resp = await getVehicles();
+  getVehicles: async (url: string) => {
+    const resp = await getVehicles(url);
+    set({ vehicles: resp });
     return resp;
   }
 }));
