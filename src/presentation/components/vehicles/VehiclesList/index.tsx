bea965531df27c1b939d10a26e37e130027af5fd
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-29 14:02:44 -0500

    Update props type

--- src/presentation/components/vehicles/VehiclesList/index.tsx
+++ src/presentation/components/vehicles/VehiclesList/index.tsx
@@ -1,16 +1,16 @@
 import { Layout, List } from '@ui-kitten/components';
 import { VehiclesListItem } from '../VehiclesListItem';
-import { VehiclesResponse } from '../../../../infrastructure/interfaces';
+import { IVehicle } from '../../../../infrastructure/interfaces';
 import { globalStyles } from '../../../styles/global.styles';
 
 interface Props {
-  vehiclesData: VehiclesResponse;
+  vehicles: IVehicle[];
 }
 
-export const VehiclesList = ({ vehiclesData }: Props) => {
+export const VehiclesList = ({ vehicles }: Props) => {
   return (
     <List
-      data={vehiclesData.vehicles}
+      data={vehicles}
       showsVerticalScrollIndicator={false}
       style={globalStyles.mainBackground}
       renderItem={({ item }) => (
