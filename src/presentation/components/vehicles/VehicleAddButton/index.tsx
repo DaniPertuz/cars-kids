ac8c489db9588a6d034afe8aaa6d0789d022cc4b
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-28 19:43:45 -0500

    Implement AddNewVehicleModal component

--- src/presentation/components/vehicles/VehicleAddButton/index.tsx
+++ src/presentation/components/vehicles/VehicleAddButton/index.tsx
@@ -1,15 +1,19 @@
+import { useState } from 'react';
 import { Layout } from '@ui-kitten/components';
-import { styles } from './styles';
 import { AddButton } from '../../ui';
+import { AddNewVehicleModal } from '../AddNewVehicleModal';
+import { styles } from './styles';
 
 interface Props {
   top: number;
 }
 
 export const VehicleAddButton = ({ top }: Props) => {
+  const [visible, setVisible] = useState(false);
   return (
     <Layout style={{ marginTop: top, ...styles.container }}>
-      <AddButton iconSize={50} onPress={() => { }} />
+      <AddButton iconSize={50} onPress={() => setVisible(true)} />
+      <AddNewVehicleModal visible={visible} setVisible={setVisible} />
     </Layout>
   );
 };
