63223635643b0d2aea10f0e3116265420ea72bdb
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-29 14:09:24 -0500

    Add Search component to navigate to SearchScreen

--- src/presentation/screens/profile/VehiclesScreen/index.tsx
+++ src/presentation/screens/profile/VehiclesScreen/index.tsx
@@ -1,19 +1,23 @@
 import { useEffect } from 'react';
 import { useWindowDimensions } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
 import { Layout } from '@ui-kitten/components';
 
-import { BackButtonContainer } from '../../../components/ui';
+import { Back, Search } from '../../../components/ui';
 import { TotalVehiclesMessage } from '../../../components/vehicles/TotalVehiclesMessage';
 import { VehicleListComponent } from '../../../components/vehicles/VehiclesListComponent';
 import { VehiclesListPagination } from '../../../components/vehicles/VehiclesListPagination';
 import { VehicleAddButton } from '../../../components/vehicles/VehicleAddButton';
 import { VehicleTitleHeader } from '../../../components/vehicles/VehicleTitleHeader';
 import { useVehiclesData } from '../../../hooks';
+import { RootStackParams } from '../../../navigation/MainNavigator';
 
 import { globalStyles } from '../../../styles/global.styles';
 
 export const VehiclesScreen = () => {
+  const navigation = useNavigation<StackNavigationProp<RootStackParams>>();
   const { top, bottom } = useSafeAreaInsets();
   const { height } = useWindowDimensions();
   const { display, vehiclesData, fetchNextPage, fetchPrevPage, getData } = useVehiclesData();
@@ -25,8 +29,9 @@ export const VehiclesScreen = () => {
   return (
     <Layout style={globalStyles.container}>
       <Layout style={{ paddingTop: height * 0.042, ...globalStyles.mainLayout }}>
-        <BackButtonContainer top={top} />
+        <Back top={top} />
         <VehicleTitleHeader />
+        <Search top={top} onPress={() => navigation.navigate('SearchScreen')} />
         <VehicleListComponent bottom={bottom} display={display} vehiclesData={vehiclesData} />
       </Layout>
       {vehiclesData.total !== 0 &&
