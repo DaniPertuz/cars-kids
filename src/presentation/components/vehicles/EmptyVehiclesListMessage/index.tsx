5e65c55458f4c3f679f08b7303c5bc82f262143d
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-29 13:57:23 -0500

    Set dynamic text for message to be displayed

--- src/presentation/components/vehicles/EmptyVehiclesListMessage/index.tsx
+++ src/presentation/components/vehicles/EmptyVehiclesListMessage/index.tsx
@@ -3,11 +3,11 @@ import { Layout } from '@ui-kitten/components';
 import { HeaderFive } from '../../ui';
 import { styles } from './styles';
 
-export const EmptyVehiclesListMessage = () => {
+export const EmptyVehiclesListMessage = ({ text }: { text: string; }) => {
   const { height } = useWindowDimensions();
   return (
     <Layout style={{ height: height * 0.7, ...styles.container }}>
-      <HeaderFive text='No hay vehículos registrados' />
+      <HeaderFive text={text} />
     </Layout>
   );
 };
