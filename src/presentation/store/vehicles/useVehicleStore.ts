e97b9dc9932e666351a742ec3c90a7fdb8be1411
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-28 19:38:10 -0500

    Implement addVehicle action

--- src/presentation/store/vehicles/useVehicleStore.ts
+++ src/presentation/store/vehicles/useVehicleStore.ts
@@ -1,10 +1,11 @@
 import { create } from 'zustand';
 import { IVehicle } from '../../../infrastructure/interfaces';
-import { getVehicles } from '../../../actions/vehicles';
+import { addVehicle, getVehicles } from '../../../actions/vehicles';
 
 export interface VehicleState {
   vehicles: IVehicle[];
   getVehicles: (url: string) => Promise<any>;
+  addVehicle: (vehicle: IVehicle) => Promise<any>;
 }
 
 export const useVehicleStore = create<VehicleState>()((set, get) => ({
@@ -12,6 +13,10 @@ export const useVehicleStore = create<VehicleState>()((set, get) => ({
   getVehicles: async (url: string) => {
     const resp = await getVehicles(url);
     set({ vehicles: resp });
+    return resp;
+  },
+  addVehicle: async (vehicle: IVehicle) => {
+    const resp = await addVehicle(vehicle);    
     return resp;
   }
 }));
