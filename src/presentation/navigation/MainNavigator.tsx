98925a07b1493e29779578beb8e26a1ec3c4f8dd
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-29 14:09:03 -0500

    Add SearchScreen to main navigator

--- src/presentation/navigation/MainNavigator.tsx
+++ src/presentation/navigation/MainNavigator.tsx
@@ -5,6 +5,7 @@ import { LoadingScreen } from '../screens/LoadingScreen';
 import { LoginScreen } from '../screens/auth/LoginScreen';
 import { RegisterScreen } from '../screens/auth/RegisterScreen';
 import { ResetPasswordScreen } from '../screens/auth/ResetPasswordScreen';
+import { SearchScreen } from '../screens/profile/SearchScreen';
 import { UpdateProfileScreen } from '../screens/profile/UpdateProfileScreen';
 import { VehiclesScreen } from '../screens/profile/VehiclesScreen';
 import { BottomNavigator } from './BottomNavigator';
@@ -15,6 +16,7 @@ export type RootStackParams = {
   LoginScreen: undefined,
   RegisterScreen: undefined,
   ResetPasswordScreen: undefined,
+  SearchScreen: undefined,
   UpdateProfileScreen: undefined,
   VehiclesScreen: undefined,
   BottomNavigator: undefined;
@@ -44,6 +46,7 @@ export const MainNavigator = () => {
         <Stack.Screen options={{ cardStyleInterpolator: fadeAnimation }} name="LoginScreen" component={LoginScreen} />
         <Stack.Screen options={{ cardStyleInterpolator: fadeAnimation }} name="RegisterScreen" component={RegisterScreen} />
         <Stack.Screen options={{ cardStyleInterpolator: fadeAnimation }} name="ResetPasswordScreen" component={ResetPasswordScreen} />
+        <Stack.Screen options={{ cardStyleInterpolator: fadeAnimation }} name="SearchScreen" component={SearchScreen} />
         <Stack.Screen options={{ cardStyleInterpolator: fadeAnimation }} name="BottomNavigator" component={BottomNavigator} />
         <Stack.Screen options={{ cardStyleInterpolator: fadeAnimation }} name="UpdateProfileScreen" component={UpdateProfileScreen} />
         <Stack.Screen options={{ cardStyleInterpolator: fadeAnimation }} name="VehiclesScreen" component={VehiclesScreen} />
