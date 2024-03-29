458000ab40b2abb8fad2c568bae7219bc0d96975
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-29 10:23:47 -0500

    Update Back component design

--- src/presentation/components/ui/Back/index.tsx
+++ src/presentation/components/ui/Back/index.tsx
@@ -1,20 +1,23 @@
 import { TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
+import { Layout } from '@ui-kitten/components';
 import { RootStackParams } from '../../../navigation/MainNavigator';
 import { CustomIcon } from '../CustomIcon';
 import { globalStyles } from '../../../styles/global.styles';
 
-export const Back = () => {
+export const Back = ({ top }: { top: number; }) => {
   const navigator = useNavigation<StackNavigationProp<RootStackParams>>();
 
   return (
-    <TouchableOpacity
-      activeOpacity={1.0}
-      onPress={() => navigator.goBack()}
-      style={globalStyles.iconSize}
-    >
-      <CustomIcon name='arrow-circle-left-outline' />
-    </TouchableOpacity>
+    <Layout style={{ ...globalStyles.Back, marginTop: top }}>
+      <TouchableOpacity
+        activeOpacity={1.0}
+        onPress={() => navigator.goBack()}
+        style={globalStyles.iconSize}
+      >
+        <CustomIcon name='arrow-circle-left-outline' />
+      </TouchableOpacity>
+    </Layout>
   );
 };
