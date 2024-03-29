7883483b4daa8d2e69fc35e0a67d626ea505321c
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-28 19:37:33 -0500

    addVehicle store action

--- src/actions/vehicles/index.ts
+++ src/actions/vehicles/index.ts
@@ -1,6 +1,6 @@
 import { AxiosError } from 'axios';
 import carskidsApi from '../../config/api/carskidsApi';
-import { VehiclesResponse } from '../../infrastructure/interfaces';
+import { IVehicle, VehiclesResponse } from '../../infrastructure/interfaces';
 
 export const getVehicles = async (url: string) => {
   try {
@@ -13,3 +13,15 @@ export const getVehicles = async (url: string) => {
     }
   }
 };
+
+export const addVehicle = async (vehicle: IVehicle) => {
+  try {
+    const { data } = await carskidsApi.post<VehiclesResponse>('/vehicles', { vehicle });
+
+    return data;
+  } catch (error: any) {
+    if (error instanceof AxiosError) {
+      return error.response?.data;
+    }
+  }
+}
