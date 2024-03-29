483fba723664d77b639efd09a34e14b6d9c624bb
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-29 00:06:15 -0500

    Add optional caption prop

--- src/presentation/components/forms/default-input/index.tsx
+++ src/presentation/components/forms/default-input/index.tsx
@@ -2,16 +2,18 @@ import { Input } from '@ui-kitten/components';
 import { globalStyles } from '../../../styles/global.styles';
 
 interface Props {
+  caption?: string;
   placeholder: string;
   value: string;
   onChangeText: (text: string) => void;
 }
 
-export const DefaultInput = ({ placeholder, value, onChangeText }: Props) => {
+export const DefaultInput = ({ caption, placeholder, value, onChangeText }: Props) => {
 
   return (
     <Input
       placeholder={placeholder}
+      caption={caption}
       keyboardType='default'
       value={value}
       onChangeText={onChangeText}
