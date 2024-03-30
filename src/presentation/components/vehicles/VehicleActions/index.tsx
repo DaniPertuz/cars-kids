b3934de7760a2348189d2da087a6900dd90bbf97
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-29 22:46:29 -0500

    Set required components props

--- src/presentation/components/vehicles/VehicleActions/index.tsx
+++ src/presentation/components/vehicles/VehicleActions/index.tsx
@@ -1,12 +1,13 @@
 import { Layout } from '@ui-kitten/components';
+import { IVehicle } from '../../../../infrastructure/interfaces';
 import { DeleteButton, EditButton } from '../../ui';
 import { styles } from './styles';
 
-export const VehicleActions = () => {
+export const VehicleActions = ({ vehicle }: { vehicle: IVehicle; }) => {
   return (
     <Layout style={styles.container}>
       <EditButton iconSize={25} />
-      <DeleteButton iconSize={25} />
+      <DeleteButton iconSize={25} vehicle={vehicle} />
     </Layout>
   );
 };
