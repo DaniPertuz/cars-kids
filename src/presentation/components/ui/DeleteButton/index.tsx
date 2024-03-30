003f5018c83db54d3ac483914d1934561b433883
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-29 22:43:07 -0500

    Configure button to open modal and set props

--- src/presentation/components/ui/DeleteButton/index.tsx
+++ src/presentation/components/ui/DeleteButton/index.tsx
@@ -1,16 +1,28 @@
+import { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
 import { Layout } from '@ui-kitten/components';
-import { CustomIcon } from '../CustomIcon';
+import { CustomIcon, DeleteModal } from '../';
+import { IVehicle } from '../../../../infrastructure/interfaces';
 import { globalColors } from '../../../theme/globalColors';
 import { styles } from './styles';
 
 interface Props {
   iconSize: number;
+  vehicle: IVehicle;
 }
 
-export const DeleteButton = ({ iconSize }: Props) => {
+export const DeleteButton = ({ iconSize, vehicle }: Props) => {
+  const [visible, setVisible] = useState(false);
+
   return (
     <Layout style={styles.container}>
-      <CustomIcon name='trash-outline' size={{ height: iconSize, width: iconSize }} fillColor={globalColors.white} />
+      <TouchableOpacity
+        activeOpacity={1.0}
+        onPress={() => setVisible(true)}
+      >
+        <CustomIcon name='trash-outline' size={{ height: iconSize, width: iconSize }} fillColor={globalColors.white} />
+      </TouchableOpacity>
+      <DeleteModal visible={visible} setVisible={setVisible} vehicle={vehicle} />
     </Layout>
   );
 };
