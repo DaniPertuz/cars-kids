6eaa8c090c0daf443815f609051bc71b54ddfcf7
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-29 22:40:33 -0500

    Modal for delete vehicle action and styles

---
+++ src/presentation/components/ui/DeleteModal/index.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import { Card, Layout, Modal } from '@ui-kitten/components';
+import Snackbar from 'react-native-snackbar';
+
+import { Callout, PrimaryButton } from '../';
+import { IVehicle } from '../../../../infrastructure/interfaces';
+import { useVehicleStore } from '../../../store/vehicles/useVehicleStore';
+
+import { styles } from './styles';
+
+interface Props {
+  vehicle?: IVehicle;
+  visible: boolean;
+  setVisible: (visible: boolean) => void;
+}
+
+export const DeleteModal = ({ vehicle, visible, setVisible }: Props) => {
+  const [loading, setLoading] = useState(false);
+
+  const { deleteVehicle } = useVehicleStore();
+
+  const handleDeleteVehicle = async () => {
+    setLoading(true);
+
+    const resp = await deleteVehicle(vehicle!);
+
+    if (resp.error) {
+      setLoading(false);
+      Snackbar.show({ text: resp.error, duration: Snackbar.LENGTH_SHORT });
+      return;
+    }
+
+    Snackbar.show({ text: 'Vehículo desactivado', duration: Snackbar.LENGTH_SHORT });
+    setVisible(false);
+  };
+
+  return (
+    <Modal
+      visible={visible}
+      backdropStyle={styles.backdrop}
+      onBackdropPress={() => setVisible(false)}
+    >
+      <Card>
+        <Layout style={styles.container}>
+          <Callout text='¿Está seguro de desactivar este vehículo?' />
+          <PrimaryButton disabled={loading} text='Desactivar' onPress={handleDeleteVehicle} />
+        </Layout>
+      </Card>
+    </Modal>
+  );
+};
