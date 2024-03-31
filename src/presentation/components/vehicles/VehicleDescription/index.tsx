721b4bdd926f857cb00a49501d4738890a17c75a
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-30 23:40:41 -0500

    Include text components and update their values and styles conditional rendering

--- src/presentation/components/vehicles/VehicleDescription/index.tsx
+++ src/presentation/components/vehicles/VehicleDescription/index.tsx
@@ -1,15 +1,22 @@
-import { Layout, Text } from '@ui-kitten/components';
+import { Layout } from '@ui-kitten/components';
+import { IStatus, IUserRole, IVehicle } from '../../../../infrastructure/interfaces';
+import { Callout, Caption } from '../../ui';
+import { useAuthStore } from '../../../store/auth/useAuthStore';
+import { globalStyles } from '../../../styles/global.styles';
 import { styles } from './styles';
-import { IVehicle } from '../../../../infrastructure/interfaces';
 
 interface Props {
   item: IVehicle;
 }
 
 export const VehicleDescription = ({ item }: Props) => {
+  const { user } = useAuthStore();
+  const isAdmin = item.status === IStatus.Active;
+
   return (
-    <Layout style={styles.descriptionContainer}>
-      <Text category='p2'>{(item.category === 'car' ? 'Carro' : 'Moto')}</Text>
+    <Layout style={{ ...styles.descriptionContainer, width: isAdmin ? '30%' : '50%'}}>
+      <Caption textColor={globalStyles.colorOnyx} text={(item.category === 'car' ? 'Carro' : 'Moto')} />
+      {user?.role === IUserRole.Admin && <Callout category='s2' text={(isAdmin ? 'Activo' : 'Inactivo')} />}
       <Layout style={{ backgroundColor: item.color, ...styles.itemColor }} />
     </Layout>
   );
