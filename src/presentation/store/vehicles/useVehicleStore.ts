c40095a7b1fbf3e4353959d7dc805d59c489b0e4
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-02 00:03:54 -0500

    Remove store variables and their actions

--- src/presentation/store/vehicles/useVehicleStore.ts
+++ src/presentation/store/vehicles/useVehicleStore.ts
@@ -3,7 +3,6 @@ import { IVehicle } from '../../../infrastructure/interfaces';
 import { addVehicle, getVehicles, deleteVehicle, updateVehicle } from '../../../actions/vehicles';
 
 export interface VehicleState {
-  vehicles: IVehicle[];
   reload: boolean;
   getVehicles: (url: string) => Promise<any>;
   addVehicle: (vehicle: IVehicle) => Promise<any>;
@@ -13,31 +12,22 @@ export interface VehicleState {
 }
 
 export const useVehicleStore = create<VehicleState>()((set, get) => ({
-  vehicles: [],
   reload: false,
   getVehicles: async (url: string) => {
     const resp = await getVehicles(url);
-    set({ vehicles: resp });
     return resp;
   },
   addVehicle: async (vehicle: IVehicle) => {
     const resp = await addVehicle(vehicle);
-    set((state) => ({ vehicles: [...state.vehicles, vehicle] }));
     return resp;
   },
   updateVehicle: async (vehicleNickname: string, vehicle: IVehicle) => {
     const resp = await updateVehicle(vehicleNickname, vehicle);
-    set((state) => ({
-      vehicles: state.vehicles.map((v) => (v.nickname === vehicleNickname ? vehicle : v))
-    }));
     return resp;
   },
   deleteVehicle: async (vehicle: IVehicle) => {
     const resp = await deleteVehicle(vehicle);
-    set((state) => ({
-      vehicles: state.vehicles.map((v) => (v.nickname === vehicle.nickname ? resp : v))
-    }));
-    return resp;
-  },
+
+    return resp;  },
   setReload: (value: boolean) => set({ reload: value })
 }));
