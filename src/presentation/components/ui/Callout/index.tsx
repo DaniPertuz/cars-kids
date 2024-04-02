8814ec707c975e783a374312473fdc403663c3ea
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-02 17:29:38 -0500

    Add textColor prop to customize text color

--- src/presentation/components/ui/Callout/index.tsx
+++ src/presentation/components/ui/Callout/index.tsx
@@ -3,7 +3,8 @@ import { globalStyles } from '../../../styles/global.styles';
 
 interface Props {
   text: string;
+  textColor?: string;
   onPress?: () => void;
 }
 
-export const Callout = ({ text, onPress }: Props) => <Text category='s1' style={globalStyles.colorOnyx} onPress={onPress}>{text}</Text>;
+export const Callout = ({ text, textColor, onPress }: Props) => <Text category='s1' style={textColor ? { color: textColor } : globalStyles.colorOnyx} onPress={onPress}>{text}</Text>;
