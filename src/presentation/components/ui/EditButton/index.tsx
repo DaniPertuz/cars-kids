60c4c3f550a02a9269f2589eedcf6af7dbe97d5c
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-02 00:10:49 -0500

    Update component to handle modal rendering

--- src/presentation/components/ui/EditButton/index.tsx
+++ src/presentation/components/ui/EditButton/index.tsx
@@ -1,16 +1,28 @@
+import { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
 import { Layout } from '@ui-kitten/components';
-import { CustomIcon } from '../CustomIcon';
+
+import { CustomIcon } from '../';
+import { IVehicle } from '../../../../infrastructure/interfaces';
+import { VehicleEntryModal } from '../../vehicles/VehicleEntryModal';
+
 import { globalColors } from '../../../theme/globalColors';
 import { styles } from './styles';
 
 interface Props {
   iconSize: number;
+  vehicle: IVehicle;
 }
 
-export const EditButton = ({ iconSize }: Props) => {
+export const EditButton = ({ iconSize, vehicle }: Props) => {
+  const [visible, setVisible] = useState(false);
+
   return (
-    <Layout style={styles.container}>
-      <CustomIcon name='edit-outline' size={{ height: iconSize, width: iconSize }} fillColor={globalColors.white} />
-    </Layout>
+    <TouchableOpacity activeOpacity={1.0} onPress={() => setVisible(true)}>
+      <Layout style={styles.container}>
+        <CustomIcon name='edit-outline' size={{ height: iconSize, width: iconSize }} fillColor={globalColors.white} />
+        <VehicleEntryModal vehicle={vehicle} visible={visible} setVisible={setVisible} />
+      </Layout>
+    </TouchableOpacity>
   );
 };
