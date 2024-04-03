91d8f855e473dfebec09e6aba560f1ae7ba79184
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-02 19:01:18 -0500

    Check auth token and status to navigate to the corresponding screen

--- src/presentation/providers/AuthProvider.tsx
+++ src/presentation/providers/AuthProvider.tsx
@@ -1,24 +1,33 @@
 import { PropsWithChildren, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
+import { StorageAdapter } from '../../config/adapters/storage-adapter';
 import { RootStackParams } from '../navigation/MainNavigator';
 import { useAuthStore } from '../store/auth/useAuthStore';
 
 export const AuthProvider = ({ children }: PropsWithChildren) => {
   const navigation = useNavigation<StackNavigationProp<RootStackParams>>();
-  const { status } = useAuthStore();
+  const { status, token } = useAuthStore();
 
-  useEffect(() => {
-    if (status !== 'checking') {
-      if (status === 'authenticated') {
-        navigation.reset({ index: 0, routes: [ {name: 'BottomNavigator' }]});
-      } else {
-        navigation.reset({ index: 0, routes: [ {name: 'LoginScreen' }]});
-      }
+  const checkToken = async () => {
+    const storedToken = await StorageAdapter.getItem('token');
+    return storedToken;
+  };
+
+  const handleTokenCheck = async () => {
+    const storedToken = await checkToken();
+    if (status === 'authenticated' || storedToken) {
+      navigation.reset({ index: 0, routes: [{ name: 'BottomNavigator' }] });
+    } else {
+      navigation.reset({ index: 0, routes: [{ name: 'LoginScreen' }] });
     }
-  }, [status]);
+  };
+  
+  useEffect(() => {
+    handleTokenCheck();
+  }, [status, token, navigation]);
 
   return (
     <>{children}</>
-  )
-}
+  );
+};
