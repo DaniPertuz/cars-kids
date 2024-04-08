e85d035dd698cf0caf166b51b8a248eeb84d2894
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-08 12:12:21 -0500

    Update actions return data type

--- src/actions/vehicles/index.ts
+++ src/actions/vehicles/index.ts
@@ -1,47 +1,51 @@
 import { AxiosError } from 'axios';
 import carskidsApi from '../../config/api/carskidsApi';
-import { IVehicle, VehicleAPIResponse } from '../../infrastructure/interfaces';
+import { IVehicle, VehicleAPIResponse, VehiclesResponse } from '../../infrastructure/interfaces';
 
-export const getVehicles = async (url: string) => {
+export const getVehicles = async (url: string): Promise<VehicleAPIResponse> => {
   try {
-    const { data } = await carskidsApi.get<VehicleAPIResponse>(url);
+    const { data } = await carskidsApi.get<VehiclesResponse>(url);
     return { response: data };
   } catch (error: any) {
     if (error instanceof AxiosError) {
       return error.response?.data;
     }
+    return { error: 'Error desconocido al realizar la solicitud' };
   }
 };
 
-export const addVehicle = async (vehicle: IVehicle) => {
+export const addVehicle = async (vehicle: IVehicle): Promise<VehicleAPIResponse> => {
   try {
     const { data } = await carskidsApi.post<VehicleAPIResponse>('vehicles', vehicle);
-    return { vehicle: data };
+    return { vehicle: data.vehicle };
   } catch (error: any) {
     if (error instanceof AxiosError) {
       return error.response?.data;
     }
+    return { error: 'Error desconocido al realizar la solicitud' };
   }
 };
 
-export const updateVehicle = async (vehicleNickname: string, vehicle: IVehicle) => {
+export const updateVehicle = async (vehicleNickname: string, vehicle: IVehicle): Promise<VehicleAPIResponse> => {
   try {
     const { data } = await carskidsApi.put<VehicleAPIResponse>(`vehicles/${vehicleNickname}`, vehicle);
-    return { vehicle: data };
+    return { vehicle: data.vehicle };
   } catch (error: any) {
     if (error instanceof AxiosError) {
       return error.response?.data;
     }
+    return { error: 'Error desconocido al realizar la solicitud' };
   }
 };
 
-export const deleteVehicle = async (vehicle: IVehicle) => {
+export const deleteVehicle = async (vehicle: IVehicle): Promise<VehicleAPIResponse> => {
   try {
     const { data } = await carskidsApi.delete<VehicleAPIResponse>(`vehicles/${vehicle.nickname}`);
-    return data;
+    return { status: data.status };
   } catch (error: any) {
     if (error instanceof AxiosError) {
       return error.response?.data;
     }
+    return { error: 'Error desconocido al realizar la solicitud' };
   }
 };
