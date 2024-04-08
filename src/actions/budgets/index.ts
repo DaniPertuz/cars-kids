83e056d33ba9902875462f3d83eac911fe4eb216
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-08 14:39:12 -0500

    Update budgets actions return type using BudgetAPIResponse interface

--- src/actions/budgets/index.ts
+++ src/actions/budgets/index.ts
@@ -1,88 +1,88 @@
 import { AxiosError } from 'axios';
 import carskidsApi from '../../config/api/carskidsApi';
-import { BudgetResponse, IBudget } from '../../infrastructure/interfaces';
+import { BudgetAPIResponse, BudgetResponse, IBudget } from '../../infrastructure/interfaces';
 
-export const getAllBudgets = async (url: string) => {
+export const getAllBudgets = async (url: string): Promise<BudgetAPIResponse> => {
   try {
     const { data } = await carskidsApi.get<BudgetResponse>(url);
-
-    return data;
+    return { response: data };
   } catch (error: any) {
     if (error instanceof AxiosError) {
       return error.response?.data;
     }
+    return { error: 'Error desconocido al realizar la solicitud' };
   }
 };
 
-export const getBudgetsByDay = async (day: string, month: string, year: string) => {
+export const getBudgetsByDay = async (day: string, month: string, year: string): Promise<BudgetAPIResponse> => {
   try {
     const { data } = await carskidsApi.get<BudgetResponse>(`budgets/dates/day/${day}/${month}/${year}`);
-
-    return data;
+    return { response: data };
   } catch (error: any) {
     if (error instanceof AxiosError) {
       return error.response?.data;
     }
+    return { error: 'Error desconocido al realizar la solicitud' };
   }
 };
 
-export const getBudgetsByMonth = async (url: string, date: string) => {
+export const getBudgetsByMonth = async (url: string, date: string): Promise<BudgetAPIResponse> => {
   try {
     const split = date.split('-');
     const { data } = await carskidsApi.get<BudgetResponse>(`${url}/${split[1]}/${split[2]}`);
-
-    return data;
+    return { response: data };
   } catch (error: any) {
     if (error instanceof AxiosError) {
       return error.response?.data;
     }
+    return { error: 'Error desconocido al realizar la solicitud' };
   }
 };
 
-export const getBudgetsByPeriod = async (url: string, startingDate: string, endingDate: string) => {
+export const getBudgetsByPeriod = async (url: string, startingDate: string, endingDate: string): Promise<BudgetAPIResponse> => {
   try {
     const { data } = await carskidsApi.get<BudgetResponse>(`${url}/${startingDate}/${endingDate}`);
-
-    return data;
+    return { response: data };
   } catch (error: any) {
     if (error instanceof AxiosError) {
       return error.response?.data;
     }
+    return { error: 'Error desconocido al realizar la solicitud' };
   }
 };
 
-export const createBudget = async (budget: IBudget) => {
+export const createBudget = async (budget: IBudget): Promise<BudgetAPIResponse> => {
   try {
-    const { data } = await carskidsApi.post<BudgetResponse>('budgets', budget);
-
-    return data;
+    const { data } = await carskidsApi.post<BudgetAPIResponse>('budgets', budget);
+    return { budget: data.budget };
   } catch (error: any) {
     if (error instanceof AxiosError) {
       return error.response?.data;
     }
+    return { error: 'Error desconocido al realizar la solicitud' };
   }
 };
 
-export const updateBudget = async (budget: IBudget) => {
+export const updateBudget = async (budget: IBudget): Promise<BudgetAPIResponse> => {
   try {
-    const { data } = await carskidsApi.put<BudgetResponse>(`budgets/${budget._id}`, budget);
-
-    return data;
+    const { data } = await carskidsApi.put<BudgetAPIResponse>(`budgets/${budget._id}`, budget);
+    return { budget: data.budget };
   } catch (error: any) {
     if (error instanceof AxiosError) {
       return error.response?.data;
     }
+    return { error: 'Error desconocido al realizar la solicitud' };
   }
 };
 
-export const deleteBudget = async (budget: IBudget) => {
+export const deleteBudget = async (budget: IBudget): Promise<BudgetAPIResponse> => {
   try {
-    const { data } = await carskidsApi.delete<BudgetResponse>(`budgets/${budget._id}`);
-
-    return data;
+    const { data } = await carskidsApi.delete<BudgetAPIResponse>(`budgets/${budget._id}`);
+    return { status: data.status };
   } catch (error: any) {
     if (error instanceof AxiosError) {
       return error.response?.data;
     }
+    return { error: 'Error desconocido al realizar la solicitud' };
   }
 };
