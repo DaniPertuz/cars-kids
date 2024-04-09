330c4eea9ce0550969c5340f3456ff3826b98ba6
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-08 20:49:37 -0500

    Implement useUserInfo custom hook

--- src/presentation/components/profile/ProfileHeader/index.tsx
+++ src/presentation/components/profile/ProfileHeader/index.tsx
@@ -1,16 +1,15 @@
 import { Layout } from '@ui-kitten/components';
 import { Footnote, Title } from '../../ui';
-import { useAuthStore } from '../../../store/auth/useAuthStore';
+import { useUserInfo } from '../../../hooks';
 import { styles } from './styles';
 
 export const ProfileHeader = () => {
-  const name = useAuthStore(state => state.user?.name!);
-  const email = useAuthStore(state => state.user?.email!);
+  const { user } = useUserInfo();
 
   return (
     <Layout style={styles.titleContainer}>
-      <Title text={name} />
-      <Footnote text={email} />
+      <Title text={user?.name!} />
+      <Footnote text={user?.email!} />
     </Layout>
   );
-}
+};
