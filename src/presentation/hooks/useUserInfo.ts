163282ec725c6cb5a2dfb6b78dfd66a5de246f28
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-08 20:48:41 -0500

    useUserInfo custom hook to handle basic user profile data

---
+++ src/presentation/hooks/useUserInfo.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import { StorageAdapter } from '../../config/adapters/storage-adapter';
+import { useAuthStore } from '../store/auth/useAuthStore';
+import { IUser } from '../../infrastructure/interfaces';
+
+export const useUserInfo = () => {
+  const [user, setUser] = useState<IUser>();
+  const userStore = useAuthStore(state => state.user);
+
+  const setUserData = async () => {
+    const userStorage = await StorageAdapter.getItem('user');
+    const userJson = JSON.parse(userStorage!);
+
+    setUser({
+      name: userStore ? userStore.name : userJson.name,
+      email: userStore ? userStore.email : userJson.email,
+      role: userStore ? userStore.role : userJson.role,
+      status: userStore ? userStore.status : userJson.status
+    });
+  };
+
+  useEffect(() => {
+    setUserData();
+  }, [userStore]);
+
+  return { user };
+};
