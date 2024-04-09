a6b5e0a44e35e69321ea5050e29f83a980e184ef
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-09 10:09:54 -0500

    ProductListComponent to display products list

---
+++ src/presentation/components/products/ProductListComponent/index.tsx
@@ -0,0 +1,29 @@
+import { Layout } from '@ui-kitten/components';
+import { ProductResponse } from '../../../../infrastructure/interfaces';
+import { LoadingScreen } from '../../../screens/LoadingScreen';
+import { EmptyListMessage } from '../../ui';
+import { ProductsList } from '../ProductsList';
+import { globalStyles } from '../../../styles/global.styles';
+
+interface Props {
+  bottom: number;
+  display: boolean;
+  productsData: ProductResponse;
+}
+
+export const ProductListComponent = ({ bottom, display, productsData }: Props) => {
+  return (
+    <Layout style={{ ...globalStyles.mainBackground, marginHorizontal: 20, paddingBottom: bottom + 150 }}>
+      {(!display)
+        ?
+        <LoadingScreen />
+        :
+        (productsData.total === 0)
+          ?
+          <EmptyListMessage text='No hay productos registrados' />
+          :
+          <ProductsList products={productsData.products} />
+      }
+    </Layout>
+  );
+};
