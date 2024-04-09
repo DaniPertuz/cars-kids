82dbbc932a6d50b383e0fab7992ec9079c37ab96
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-08 20:48:54 -0500

    Add useUserInfo to index file

--- src/presentation/hooks/index.ts
+++ src/presentation/hooks/index.ts
@@ -2,4 +2,5 @@ export * from './useBudgetData';
 export * from './useDebouncedValue';
 export * from './useEmptyFieldValidation';
 export * from './useResetProfile';
+export * from './useUserInfo';
 export * from './useVehiclesData';
