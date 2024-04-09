f718df3261c6f644a100f15327dfe583ce6c0e45
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-09 11:01:02 -0500

    Update component to receive props from navigation and add conditional search component rendering

--- src/presentation/screens/profile/SearchScreen/index.tsx
+++ src/presentation/screens/profile/SearchScreen/index.tsx
@@ -1,13 +1,19 @@
 import { Layout } from '@ui-kitten/components';
 import { useWindowDimensions } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { StackScreenProps } from '@react-navigation/stack';
 
 import { Back } from '../../../components/ui';
+import { ProductsSearch } from '../../../components/products/ProductsSearch';
 import { VehiclesSearch } from '../../../components/vehicles/VehiclesSearch';
+import { RootStackParams } from '../../../navigation/MainNavigator';
 
 import { globalStyles } from '../../../styles/global.styles';
 
-export const SearchScreen = () => {
+interface Props extends StackScreenProps<RootStackParams, 'SearchScreen'> { };
+
+export const SearchScreen = ({ route }: Props) => {
+  const { entity } = route.params;
   const { top } = useSafeAreaInsets();
   const { height } = useWindowDimensions();
 
@@ -15,7 +21,7 @@ export const SearchScreen = () => {
     <Layout style={globalStyles.container}>
       <Layout style={{ paddingTop: height * 0.042, ...globalStyles.mainLayout }}>
         <Back top={top} />
-        <VehiclesSearch />
+        {entity === 'vehicles' ? <VehiclesSearch /> : <ProductsSearch />}
       </Layout>
     </Layout>
   );
