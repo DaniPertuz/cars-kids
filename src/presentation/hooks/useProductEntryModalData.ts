9527bd6eaa9e24f685c7c7eee7ea191cad9c3f83
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-10 10:56:21 -0500

    useProductEntryModalData custom hook to handle ProductEntryModal component data

---
+++ src/presentation/hooks/useProductEntryModalData.ts
@@ -0,0 +1,90 @@
+import { useState, useEffect } from 'react';
+import Snackbar from 'react-native-snackbar';
+import { updateProduct, addProduct } from '../../actions/products';
+import { IProduct, IStatus, IUserRole } from '../../infrastructure/interfaces';
+import { useUserInfo } from './useUserInfo';
+
+interface Props {
+  product?: IProduct;
+  visible: boolean;
+  setVisible: (visible: boolean) => void;
+}
+
+export const useProductEntryModalData = ({ product, visible, setVisible }: Props) => {
+  const init: IProduct = {
+    _id: '',
+    name: '',
+    cost: 0,
+    price: 0,
+    status: IStatus.Active,
+  };
+  const [loading, setLoading] = useState(false);
+  const [productState, setProductState] = useState<IProduct>({
+    _id: product?._id || '',
+    name: product?.name || '',
+    cost: product?.cost || 0,
+    price: product?.price || 0,
+    status: product?.status || IStatus.Active
+  });
+  const { user } = useUserInfo();
+  const isAdmin = user?.role === IUserRole.Admin;
+
+  const handleFieldChange = (fieldName: keyof IProduct, value: string | number) => {
+    setProductState(prevState => ({
+      ...prevState,
+      [fieldName]: value
+    }));
+  };
+
+  const handleStatus = (status: number) => {
+    switch (status) {
+      case 0:
+        setProductState({
+          ...productState,
+          status: IStatus.Active
+        });
+        break;
+      case 1:
+        setProductState({
+          ...productState,
+          status: IStatus.Inactive
+        });
+        break;
+    }
+  };
+
+  const onSubmit = async () => {
+    setLoading(true);
+
+    const resp = product ? await updateProduct(product.name, productState) : await addProduct(productState);
+
+    if (resp.error) {
+      setLoading(false);
+      Snackbar.show({ text: resp.error, duration: Snackbar.LENGTH_SHORT });
+      return;
+    }
+
+    const actionText = product ? 'actualizado' : 'registrado';
+    const successMessage = `Producto ${actionText} exitosamente`;
+
+    setLoading(false);
+    Snackbar.show({ text: successMessage, duration: Snackbar.LENGTH_SHORT });
+    setVisible(false);
+    setProductState(product ? productState : init);
+  };
+
+  useEffect(() => {
+    if (!product && !visible) {
+      setProductState({ ...productState, name: '' });
+    }
+  }, [product, visible]);
+
+  return {
+    loading,
+    productState,
+    isAdmin,
+    handleFieldChange,
+    handleStatus,
+    onSubmit
+  }
+}
