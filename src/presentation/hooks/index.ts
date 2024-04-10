b0c875d0d9910a7c7f96a023ba9ea167e57d6e2e
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-10 10:48:39 -0500

    Add useVehicleEntryModalData to index file

--- src/presentation/hooks/index.ts
+++ src/presentation/hooks/index.ts
@@ -4,4 +4,5 @@ export * from './useEmptyFieldValidation';
 export * from './useProductsData';
 export * from './useResetProfile';
 export * from './useUserInfo';
+export * from './useVehicleEntryModalData';
 export * from './useVehiclesData';
