ae604a80f725697aa52191d8be1864ce76955bb5
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-10 18:18:20 -0500

    Add Cloudinary env variables

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -3,4 +3,8 @@ declare module '@env' {
   export const API_URL: string;
   export const API_URL_ANDROID: string;
   export const API_URL_IOS: string;
+  export const CLOUDINARY_API_KEY: string;
+  export const CLOUDINARY_API_SECRET: string;
+  export const CLOUDINARY_CLOUD_NAME: string;
+  export const CLOUDINARY_UPLOAD_PRESET: string;
 }
