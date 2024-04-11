4b811890ba3a48dee440a7049f1a6ae1cca8e527
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-11 14:14:50 -0500

    Include custom Profile components and add conditional user image rendering

--- src/presentation/components/profile/ProfileUserImage/index.tsx
+++ src/presentation/components/profile/ProfileUserImage/index.tsx
@@ -1,17 +1,24 @@
+import { useState } from 'react';
 import { Image } from 'react-native';
 import { Layout } from '@ui-kitten/components';
-import { CustomIcon } from '../../ui';
+
+import { ProfileEditUserImageButton } from '../ProfileEditUserImageButton';
+import { ProfileUserImageModal } from '../ProfileUserImageModal';
+import { useUserInfo } from '../../../hooks';
+
 import { styles } from './styles';
 
 export const ProfileUserImage = ({ height }: { height: number; }) => {
+  const [visible, setVisible] = useState(false);
+  const { user } = useUserInfo();
+
   return (
     <Layout style={styles.imageContainer}>
-      <Image style={styles.image} source={require('../../../../assets/logo2.png')} />
+      <Image style={styles.image} source={user?.img ? { uri: user.img } : require('../../../../assets/logo2.png')} />
       <Layout style={{ ...styles.editImageIconContainer, top: height * 0.13 }}>
-        <Layout style={styles.editImageIconBackground}>
-          <CustomIcon name='edit-outline' size={styles.editImageIconSize} />
-        </Layout>
+        <ProfileEditUserImageButton onPress={() => setVisible(true)} />
       </Layout>
+      <ProfileUserImageModal visible={visible} setVisible={setVisible} />
     </Layout>
   );
 };
