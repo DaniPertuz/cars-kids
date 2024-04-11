59b870530b2809b9996d8bd0fdf05d464a69c5ea
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-11 14:44:41 -0500

    Implement Users API response interfaces to define functions return type

--- src/actions/users/index.ts
+++ src/actions/users/index.ts
@@ -1,50 +1,54 @@
 import { AxiosError } from 'axios';
 import carskidsApi from '../../config/api/carskidsApi';
-import { IUser } from '../../infrastructure/interfaces';
+import { IUser, UserAPIResponse } from '../../infrastructure/interfaces';
 
-export const updateUserName = async (email: string, name: string) => {
+export const updateUserName = async (email: string, name: string): Promise<UserAPIResponse> => {
   try {
     const { data } = await carskidsApi.put<IUser>('users/name', { email, name });
-    return data;
+    return { user: data };
   } catch (error) {
     if (error instanceof AxiosError) {
       return error.response?.data;
     }
+    return { error: 'Error desconocido al realizar la solicitud' };
   }
 };
 
-export const updateUserImage = async (email: string, img: string) => {
+export const updateUserImage = async (email: string, img: string): Promise<UserAPIResponse> => {
   try {
     const { data } = await carskidsApi.put<IUser>('users/image', { email, img });
 
-    return data;
+    return { user: data };
   } catch (error) {
     if (error instanceof AxiosError) {
       return error.response?.data;
     }
+    return { error: 'Error desconocido al realizar la solicitud' };
   }
 };
 
-export const updateUserEmail = async (email: string, newEmail: string) => {
+export const updateUserEmail = async (email: string, newEmail: string): Promise<UserAPIResponse> => {
   try {
     const { data } = await carskidsApi.put<IUser>('users/email', { email, newEmail });
 
-    return data;
+    return { user: data };
   } catch (error) {
     if (error instanceof AxiosError) {
       return error.response?.data;
     }
+    return { error: 'Error desconocido al realizar la solicitud' };
   }
 };
 
-export const updateUserPassword = async (email: string, password: string) => {
+export const updateUserPassword = async (email: string, password: string): Promise<UserAPIResponse> => {
   try {
-    const { data } = await carskidsApi.put('/users/password', { email, password })
+    const { data } = await carskidsApi.put('/users/password', { email, password });
 
-    return data;
+    return { user: data };
   } catch (error) {
     if (error instanceof AxiosError) {
       return error.response?.data;
     }
+    return { error: 'Error desconocido al realizar la solicitud' };
   }
-}
+};
