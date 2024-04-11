5bb1004ac64f495a05cc6f4313a619240b6ccff7
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-11 14:13:47 -0500

    ProfileUserImageModal Profile component to handle user profile image

---
+++ src/presentation/components/profile/ProfileUserImageModal/index.tsx
@@ -0,0 +1,52 @@
+import { Image } from 'react-native';
+import { Card, Layout, Modal } from '@ui-kitten/components';
+import { Headline, PrimaryButton } from '../../ui';
+import { useProfileUserImage, useUserInfo } from '../../../hooks';
+import { globalStyles } from '../../../styles/global.styles';
+import { globalColors } from '../../../theme/globalColors';
+import { useEffect, useState } from 'react';
+
+interface Props {
+  visible: boolean;
+  setVisible: (value: boolean) => void;
+}
+
+export const ProfileUserImageModal = ({ visible, setVisible }: Props) => {
+  const { user } = useUserInfo();
+  const [displayThumb, setDisplayThumb] = useState(false);
+  const { loading, profilePicture, setProfilePicture, selectPicture, takePicture, onSubmit } = useProfileUserImage();
+
+  useEffect(() => {
+    const hasProfileAssets = profilePicture?.assets !== undefined;
+    if (user?.img) {
+      setDisplayThumb(false);
+    } else if (visible) {
+      setDisplayThumb(!hasProfileAssets);
+    } else {
+      setDisplayThumb(hasProfileAssets);
+      setProfilePicture(undefined);
+    }
+  }, [visible, profilePicture, user]);
+
+  const profileImagePath = displayThumb
+    ? require('../../../../assets/logo2.png')
+    : { uri: profilePicture ? profilePicture?.assets?.[0]?.uri : user?.img };
+
+  return (
+    <Modal visible={visible} backdropStyle={globalStyles.backdrop} onBackdropPress={() => setVisible(false)}>
+      <Card>
+        <Layout style={globalStyles.modalContainer}>
+          <Headline text={'Imagen'} textColor={globalStyles.colorOnyx} />
+          <Layout style={globalStyles.alignJustifyCenter}>
+            <Image style={{ height: 320, width: 320, borderRadius: 50 }} source={profileImagePath} />
+          </Layout>
+          <Layout style={{ justifyContent: 'center', flexDirection: 'row', gap: 20 }}>
+            <PrimaryButton disabled={false} text={'Foto'} color={globalColors.warning} onPress={takePicture} />
+            <PrimaryButton disabled={false} text={'Galería'} color={globalColors.success} onPress={selectPicture} />
+          </Layout>
+          <PrimaryButton disabled={loading} text={'Actualizar'} onPress={onSubmit} />
+        </Layout>
+      </Card>
+    </Modal>
+  );
+};
