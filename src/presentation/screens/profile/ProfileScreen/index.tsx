a83248e26c51c23959960149cc1036ae731af88d
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-11 14:15:47 -0500

    Perform conditional rendering depending on user data

--- src/presentation/screens/profile/ProfileScreen/index.tsx
+++ src/presentation/screens/profile/ProfileScreen/index.tsx
@@ -2,23 +2,31 @@ import { useWindowDimensions } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Layout } from '@ui-kitten/components';
 
+import { LoadingScreen } from '../../LoadingScreen';
 import { ProfileHeader, ProfileMenu, ProfileUserImage } from '../../../components/profile';
 import { Back } from '../../../components/ui';
+import { useUserInfo } from '../../../hooks';
 
 import { globalStyles } from '../../../styles/global.styles';
 
 export const ProfileScreen = () => {
   const { top } = useSafeAreaInsets();
   const { height } = useWindowDimensions();
+  const { user } = useUserInfo();
 
   return (
     <Layout style={globalStyles.container}>
       <Back top={top} />
-      <Layout style={{ paddingTop: height * 0.08, ...globalStyles.mainLayout }}>
-        <ProfileUserImage height={height} />
-        <ProfileHeader />
-        <ProfileMenu />
-      </Layout>
+      {!user
+        ?
+        <LoadingScreen />
+        :
+        <Layout style={{ paddingTop: height * 0.08, ...globalStyles.mainLayout }}>
+          <ProfileUserImage height={height} />
+          <ProfileHeader />
+          <ProfileMenu />
+        </Layout>
+      }
     </Layout>
   );
 };
