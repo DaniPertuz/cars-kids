1cb00cb18c9b5e3e8be2b31e35380272864f97a1
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-12 19:23:35 -0500

    VehicleImageModal component and styles to update vehicle image

---
+++ src/presentation/components/vehicles/VehicleImageModal/index.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react';
+import { Image } from 'react-native';
+import { Card, Layout, Modal } from '@ui-kitten/components';
+import { HeaderFive, PrimaryButton } from '../../ui';
+import { useVehicleImage } from '../../../hooks';
+import { Vehicle } from '../../../../core/entities';
+import { globalStyles } from '../../../styles/global.styles';
+import { globalColors } from '../../../theme/globalColors';
+import { styles } from './styles';
+
+interface Props {
+  vehicle: Vehicle
+  visible: boolean;
+  setVisible: (value: boolean) => void;
+}
+
+export const VehicleImageModal = ({ vehicle, visible, setVisible }: Props) => {
+  const [displayThumb, setDisplayThumb] = useState(false);
+  const { loading, vehiclePicture, setVehiclePicture, selectPicture, takePicture, onSubmit } = useVehicleImage({ vehicle, setVisible });
+
+  useEffect(() => {
+    const hasVehicleAssets = vehiclePicture?.assets !== undefined;
+    if (vehicle.img) {
+      setDisplayThumb(false);
+    } else if (visible) {
+      setDisplayThumb(!hasVehicleAssets);
+    } else {
+      setDisplayThumb(hasVehicleAssets);
+      setVehiclePicture(undefined);
+    }
+  }, [visible, vehiclePicture]);
+
+  const vehicleImagePath = displayThumb
+    ? require('../../../../assets/logo2.png')
+    : { uri: (vehiclePicture && vehiclePicture.assets && vehiclePicture.assets[0] && vehiclePicture.assets[0].uri && vehiclePicture.assets[0].uri.trim() !== '') ? vehiclePicture.assets[0].uri : (vehicle.img && vehicle.img.trim() !== '' ? vehicle.img : undefined) };
+
+  return (
+    <Modal visible={visible} backdropStyle={globalStyles.backdrop} onBackdropPress={() => setVisible(false)}>
+      <Card>
+        <Layout style={globalStyles.modalContainer}>
+          <HeaderFive text={`Imagen de ${vehicle.nickname}`} />
+          <Layout style={globalStyles.alignJustifyCenter}>
+            <Image style={styles.vehicleImage} source={vehicleImagePath} />
+          </Layout>
+          <Layout style={styles.buttonsContainer}>
+            <PrimaryButton disabled={false} text={'Foto'} color={globalColors.warning} onPress={takePicture} />
+            <PrimaryButton disabled={false} text={'Galería'} color={globalColors.success} onPress={selectPicture} />
+          </Layout>
+          <PrimaryButton disabled={loading} text={'Actualizar'} onPress={onSubmit} />
+        </Layout>
+      </Card>
+    </Modal>
+  );
+};
---
+++ src/presentation/components/vehicles/VehicleImageModal/styles.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    gap: 20
+  },
+  vehicleImage: {
+    borderRadius: 50,
+    height: 320,
+    width: 320
+  }
+});
