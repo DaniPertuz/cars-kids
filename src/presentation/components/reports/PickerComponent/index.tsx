a47645d55eebf6c456eabb90589a37dd45742d9c
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-16 09:55:42 -0500

    PickerComponent Reports component to group pickers

---
+++ src/presentation/components/reports/PickerComponent/index.tsx
@@ -0,0 +1,25 @@
+import { DayPicker } from '../DayPicker';
+import { MonthPicker } from '../MonthPicker';
+import { PeriodPicker } from '../PeriodPicker';
+
+interface Props {
+  lapse: string;
+  dayDate: Date | undefined;
+  dayDateText: string;
+  setDayDate: (value: Date | undefined) => void;
+  handleMonthYear: (type: 'month' | 'year', value: string) => void;
+  handlePeriod: (startDate: string, endDate: string) => void;
+}
+
+export const PickerComponent = ({ lapse, dayDate, dayDateText, setDayDate, handleMonthYear, handlePeriod }: Props) => {
+  switch (lapse) {
+    case 'Día':
+      return <DayPicker date={dayDate} dateText={dayDateText} setDate={setDayDate} />;
+    case 'Mes':
+      return <MonthPicker handleMonthYear={handleMonthYear} />;
+    case 'Personalizado':
+      return <PeriodPicker handleSelectedRange={handlePeriod} />;
+    default:
+      return null;
+  }
+};
