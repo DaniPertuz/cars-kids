859a4f150bfcb1039cd1e6090f059b312356884d
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-16 09:38:49 -0500

    I18nConfig utils for days and months handling

---
+++ src/utils/i18nConfig.ts
@@ -0,0 +1,12 @@
+import { I18nConfig } from '@ui-kitten/components';
+
+export const i18n: I18nConfig = {
+  dayNames: {
+    short: ['Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sá', 'Do'],
+    long: ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo']
+  },
+  monthNames: {
+    short: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+    long: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+  }
+};
