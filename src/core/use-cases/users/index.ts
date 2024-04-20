446130778818db81010d8557a9138beeadbd8c37
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-20 00:21:56 -0500

    updateUserRoleUseCase use case to handle update user role process

--- src/core/use-cases/users/index.ts
+++ src/core/use-cases/users/index.ts
@@ -4,3 +4,4 @@ export * from './update-user-email.use-case';
 export * from './update-user-image.use-case';
 export * from './update-user-name.use-case';
 export * from './update-user-password.use-case';
+export * from './update-user-role.use-case';
