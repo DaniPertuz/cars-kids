222964aace8dceb53e67132ecb70ee6e677a149d
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-20 00:44:16 -0500

    Add UserEntryModal component and params to handle edit process for users

--- src/presentation/components/ui/EditButton/index.tsx
+++ src/presentation/components/ui/EditButton/index.tsx
@@ -4,8 +4,9 @@ import { Layout } from '@ui-kitten/components';
 
 import { CustomIcon } from '../';
 import { ProductEntryModal } from '../../products/ProductEntryModal';
+import { UserEntryModal } from '../../users/UserEntryModal';
 import { VehicleEntryModal } from '../../vehicles/VehicleEntryModal';
-import { Product, Vehicle } from '../../../../core/entities';
+import { Product, User, Vehicle } from '../../../../core/entities';
 
 import { globalColors } from '../../../theme/globalColors';
 import { styles } from './styles';
@@ -13,10 +14,11 @@ import { styles } from './styles';
 interface Props {
   iconSize: number;
   product?: Product;
+  user?: User;
   vehicle?: Vehicle;
 }
 
-export const EditButton = ({ iconSize, product, vehicle }: Props) => {
+export const EditButton = ({ iconSize, product, user, vehicle }: Props) => {
   const [visible, setVisible] = useState(false);
 
   return (
@@ -24,6 +26,7 @@ export const EditButton = ({ iconSize, product, vehicle }: Props) => {
       <Layout style={styles.container}>
         <CustomIcon name='edit-outline' size={{ height: iconSize, width: iconSize }} fillColor={globalColors.white} />
         {product && <ProductEntryModal product={product} visible={visible} setVisible={setVisible} />}
+        {user && <UserEntryModal user={user} visible={visible} setVisible={setVisible} />}
         {vehicle && <VehicleEntryModal vehicle={vehicle} visible={visible} setVisible={setVisible} />}
       </Layout>
     </TouchableOpacity>
