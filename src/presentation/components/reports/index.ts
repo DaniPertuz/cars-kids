71c5ce647dd224e27ae78235442a297b1dd96c6a
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-20 00:43:25 -0500

    Add report components to index file

--- src/presentation/components/reports/index.ts
+++ src/presentation/components/reports/index.ts
@@ -3,3 +3,6 @@ export * from './EditSelectedDate';
 export * from './MonthPicker';
 export * from './PeriodPicker';
 export * from './PickerComponent';
+export * from './ReportsList';
+export * from './ReportsListComponent';
+export * from './ReportsSelectComponentsGroup';
