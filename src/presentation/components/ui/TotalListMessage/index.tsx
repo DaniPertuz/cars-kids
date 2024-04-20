6cb0f11c4c3ac365ac3013e30e77e108f9df3a04
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-20 00:19:38 -0500

    Update entity name formatting

--- src/presentation/components/ui/TotalListMessage/index.tsx
+++ src/presentation/components/ui/TotalListMessage/index.tsx
@@ -9,9 +9,22 @@ interface Props {
 }
 
 export const TotalListMessage = ({ bottom, item, total }: Props) => {
+  const formatItemName = (word: string, count: number) => {
+    switch (word) {
+      case 'Alquileres':
+        return count === 1 ? 'Alquiler' : 'Alquileres';
+      case 'Presupuestos':
+        return count === 1 ? 'Presupuesto' : 'Presupuestos';
+      case 'Usuarios':
+        return count === 1 ? 'Usuario' : 'Usuarios';
+      default:
+        return count === 1 ? word.slice(0, -1) : `${word}s`;
+    }
+  };
+
   return (
     <Layout style={{ ...styles.container, bottom: bottom + 20 }}>
-      <Callout text={`${total} ${total === 1 ? (item === 'Alquileres' ? 'Alquiler' : item.slice(0, -1)) : (item === 'Alquileres' ? 'Alquileres' : `${item}s`)}`} />
+      <Callout text={`${total} ${formatItemName(item, total)}`} />
     </Layout>
   );
 };
