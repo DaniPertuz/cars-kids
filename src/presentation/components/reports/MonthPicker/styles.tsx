f719577447e19a470bb3871e2bd52a34eec431eb
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-20 00:33:18 -0500

    Add new style rules for MonthPicker component

--- src/presentation/components/reports/MonthPicker/styles.tsx
+++ src/presentation/components/reports/MonthPicker/styles.tsx
@@ -1,10 +1,21 @@
 import { StyleSheet } from 'react-native';
 import { globalColors } from '../../../theme/globalColors';
+import { globalStyles } from '../../../styles/global.styles';
 
 export const styles = StyleSheet.create({
   container: {
     backgroundColor: globalColors.background,
     flexDirection: 'row',
     justifyContent: 'space-between'
+  },
+  resetDataContainer: {
+    ...globalStyles.mainBackground,
+    marginHorizontal: 5
+  },
+  selectsContainer: {
+    alignSelf: 'center',
+    backgroundColor: globalColors.background,
+    flexDirection: 'row',
+    justifyContent: 'center'
   }
 });
