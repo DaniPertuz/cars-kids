62eba0546141ad27e109ea602efafa408be1bc30
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-20 00:51:38 -0500

    Add ReportsScreen screen to profile menu and display option if user role is admin

--- src/presentation/components/profile/ProfileMenu/index.tsx
+++ src/presentation/components/profile/ProfileMenu/index.tsx
@@ -2,6 +2,8 @@ import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { Layout } from '@ui-kitten/components';
 
+import { useUserInfo } from '../../../hooks';
+import { IUserRole } from '../../../../infrastructure/interfaces';
 import { RootStackParams } from '../../../navigation/MainNavigator';
 import { ProfileMenuItem } from '../ProfileMenuItem';
 import { LogoutComponent } from '../Logout';
@@ -10,6 +12,7 @@ import { styles } from './styles';
 
 export const ProfileMenu = () => {
   const navigation = useNavigation<StackNavigationProp<RootStackParams>>();
+  const { user } = useUserInfo();
 
   return (
     <Layout style={styles.menuContainer}>
@@ -18,6 +21,7 @@ export const ProfileMenu = () => {
         <ProfileMenuItem iconName={'car-outline'} label={'Vehículos'} onPress={() => navigation.navigate('VehiclesScreen')} />
         <ProfileMenuItem iconName={'credit-card-outline'} label={'Presupuesto'} onPress={() => navigation.navigate('BudgetScreen')} />
         <ProfileMenuItem iconName={'archive-outline'} label={'Productos'} onPress={() => navigation.navigate('ProductsScreen')} />
+        {user?.role === IUserRole.Admin && <ProfileMenuItem iconName={'file-text-outline'} label={'Reportes'} onPress={() => navigation.navigate('ReportsScreen')} />}
       </Layout>
       <LogoutComponent />
     </Layout>
