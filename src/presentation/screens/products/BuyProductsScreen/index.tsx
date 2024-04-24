b0dce3a56fa74be003fea19dbffd77c41073e339
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-24 00:34:55 -0500

    BuyProductsScreen

---
+++ src/presentation/screens/products/BuyProductsScreen/index.tsx
@@ -0,0 +1,8 @@
+import { Layout } from '@ui-kitten/components';
+import { globalStyles } from '../../../styles/global.styles';
+
+export const BuyProductsScreen = () => {
+  return (
+    <Layout style={globalStyles.container}></Layout>
+  );
+};
