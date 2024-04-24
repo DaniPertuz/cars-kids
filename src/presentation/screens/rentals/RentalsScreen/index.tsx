2408ea43f40536dfeb295b08c2ce666552e740a5
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-24 00:34:46 -0500

    RentalsScreen

---
+++ src/presentation/screens/rentals/RentalsScreen/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export const RentalsScreen = () => {
+  return (
+    <View>
+      <Text>RentalsScreen</Text>
+    </View>
+  );
+};
