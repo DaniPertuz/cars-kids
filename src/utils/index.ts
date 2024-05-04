321d7722dc3a3cecda85bad1447e87af56a7975e
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-05-03 21:15:21 -0500

    Add utils to index file

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,6 @@
+export * from './base64Image';
 export * from './i18nConfig';
 export * from './localeDateService';
 export * from './padTo2Digits';
+export * from './PaymentDescription';
+export * from './reportUtils';
