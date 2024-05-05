d7734d3c6426d8957a8ec134168bafa4d388af54
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-05-05 14:42:29 -0500

    Add fields validation before update

--- src/presentation/screens/auth/ResetPasswordScreen/index.tsx
+++ src/presentation/screens/auth/ResetPasswordScreen/index.tsx
@@ -2,11 +2,12 @@ import { useState } from 'react';
 import { ScrollView, useWindowDimensions } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Layout } from '@ui-kitten/components';
+import Snackbar from 'react-native-snackbar';
 
 import { EmailInput, PasswordInput } from '../../../components/forms';
 import { LoginButtonContainer, LoginMainImage } from '../../../components/login';
 import { Back, Footnote, Headline } from '../../../components/ui';
-import { useResetProfile } from '../../../hooks';
+import { useEmailValidation, useResetProfile } from '../../../hooks';
 
 import { authStyles } from '../../../styles/auth/styles';
 import { globalStyles } from '../../../styles/global.styles';
@@ -16,9 +17,35 @@ export const ResetPasswordScreen = () => {
   const { height } = useWindowDimensions();
   const [loading, setLoading] = useState(false);
   const { form, email, password, confirmPassword, setForm, onUpdateProfile } = useResetProfile();
+  const isValidEmail = useEmailValidation(email);
 
-  const onSubmit = () => {    
+  const onSubmit = () => {
     setLoading(true);
+
+    if (!isValidEmail) {
+      Snackbar.show({ text: 'Email no válido', duration: Snackbar.LENGTH_SHORT });
+      setLoading(false);
+      return;
+    }
+
+    if (!email || email.length === 0) {
+      Snackbar.show({ text: 'Ingrese email', duration: Snackbar.LENGTH_SHORT });
+      setLoading(false);
+      return;
+    }
+
+    if ((!password || !confirmPassword) || (password.length === 0 || confirmPassword.length === 0)) {
+      Snackbar.show({ text: 'No se permite contraseña vacía', duration: Snackbar.LENGTH_SHORT });
+      setLoading(false);
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      Snackbar.show({ text: 'Contraseñas no coinciden', duration: Snackbar.LENGTH_SHORT });
+      setLoading(false);
+      return;
+    }
+
     onUpdateProfile();
     setLoading(false);
   };
