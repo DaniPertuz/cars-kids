ea43dfcb4101b1314285690a65044a4435086a86
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-05-05 16:47:12 -0500

    Add component styles, replace AddButton by FAB, and add variables for conditional rendering

--- src/presentation/components/reports/ReportsListPagination/index.tsx
+++ src/presentation/components/reports/ReportsListPagination/index.tsx
@@ -1,21 +1,29 @@
 import { Layout } from '@ui-kitten/components';
-import { ApiResponse } from '../../../../infrastructure/interfaces';
-import { TotalListMessage, ListPagination } from '../../ui';
-import { globalStyles } from '../../../styles/global.styles';
+import { ApiResponse, DateRange } from '../../../../infrastructure/interfaces';
+import { TotalListMessage, ListPagination, FAB } from '../../ui';
+import { useGeneratePDF } from '../../../hooks';
+import { styles } from './styles';
 
 interface Props {
   bottom: number;
   category: string;
   entityData: any;
+  lapse: string;
+  reportLapse: string;
+  range: DateRange;
   fetchNextPage: () => Promise<void>;
   fetchPrevPage: () => Promise<void>;
 }
 
-export const ReportsListPagination = ({ bottom, category, entityData, fetchNextPage, fetchPrevPage }: Props) => {
+export const ReportsListPagination = ({ bottom, category, entityData, lapse, reportLapse, range, fetchNextPage, fetchPrevPage }: Props) => {
+  const { generatePDF } = useGeneratePDF({ category, lapse, reportLapse, range, total: entityData.response.total });
+  const isPrintableCategory = category !== 'Usuarios' && category !== 'Presupuestos';
+
   return (
-    <Layout style={{ ...globalStyles.mainBackground, height: 70 }}>
-      <TotalListMessage bottom={bottom} item={category} total={entityData.response.total} />
-      <ListPagination<ApiResponse> bottom={bottom} data={entityData.response} fetchPrevPage={fetchPrevPage} fetchNextPage={fetchNextPage} />
+    <Layout style={styles.container}>
+      <TotalListMessage bottom={bottom} item={category} total={entityData?.response.total} />
+      <ListPagination<ApiResponse> bottom={bottom} data={entityData?.response} fetchPrevPage={fetchPrevPage} fetchNextPage={fetchNextPage} />
+      {isPrintableCategory && <FAB iconName='printer-outline' iconSize={40} style={styles.fab} onPress={generatePDF} />}
     </Layout>
   );
 };
