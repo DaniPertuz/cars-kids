517cab5b33e8d15a8c80a96e2818555769c73432
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-05-05 16:36:52 -0500

    Add desk and exception fields

--- src/core/entities/rental.ts
+++ src/core/entities/rental.ts
@@ -1,14 +1,17 @@
 import { IPayment } from '../../infrastructure/interfaces';
+import { Desk } from './desk';
 import { User } from './user';
 import { Vehicle } from './vehicle';
 
 export interface Rental {
-  _id?:    string;
-  client:  string;
-  time:    number;
-  date:    Date;
-  vehicle: Vehicle;
-  payment: IPayment;
-  amount:  number;
-  user:    User;
+  _id?:       string;
+  client:     string;
+  time:       number;
+  date:       Date;
+  vehicle:    Vehicle;
+  payment:    IPayment;
+  amount:     number;
+  desk:       Desk;
+  user:       User;
+  exception?: string;
 }
