592a6080c5a1d4eb6f1b7a50531ae8a0e03dc802
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-05-05 14:41:30 -0500

    Hook to validate email field value has correct email format

---
+++ src/presentation/hooks/useEmailValidation.ts
@@ -0,0 +1,4 @@
+export const useEmailValidation = (email: string): boolean => {
+  const emailRegexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+  return emailRegexp.test(email);
+};
