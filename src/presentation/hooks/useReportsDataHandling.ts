952df05b4a6ba7f319176226920a8cde2b2e0128
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-05-05 16:16:21 -0500

    Update the validation of the data that will be displayed in the reports depending on the values of the category, date ranges and lapse variables

--- src/presentation/hooks/useReportsDataHandling.ts
+++ src/presentation/hooks/useReportsDataHandling.ts
@@ -2,21 +2,32 @@ import { useState, useEffect } from 'react';
 import { TranslationWidth } from '@ui-kitten/components';
 import { useEntityData } from './useEntityData';
 import { useFormattedDate } from './useFormattedDate';
+import { DateRange } from '../../infrastructure/interfaces';
 import { padTo2Digits, i18n } from '../../utils';
 
 export const useReportsDataHandling = () => {
   const [category, setCategory] = useState<string>('');
   const [lapse, setLapse] = useState<string>('');
-  const [dayRange, setDayRange] = useState({ day: '', month: '', year: '' });
-  const [monthRange, setMonthRange] = useState({ month: '', year: '' });
-  const [range, setRange] = useState({ startDate: '', endDate: '' });
+  const [reportLapse, setReportLapse] = useState<string>('');
+  const [range, setRange] = useState<DateRange>({ day: undefined, month: undefined, year: undefined, startDate: undefined, endDate: undefined });
 
-  const { date: dayDate, setDate: setDayDate, dateNumbersText: dayDateNumbersText, dateText: dayDateText } = useFormattedDate();
+  const { display, entityData, fetchData, fetchNextPage, fetchPrevPage, setEntityData }
+    = useEntityData({
+      category,
+      rangeType: lapse,
+      range: lapse === 'Día'
+        ? { day: range.day, month: range.month, year: range.year }
+        : lapse === 'Mes'
+          ? { month: range.month, year: range.year }
+          : { startDate: range.startDate, endDate: range.endDate }
+    });
+  const { date: dayDate, setDate: setDayDate, dateNumbersText: dayDateNumbersText, dateText: dayDateText, formatDateString } = useFormattedDate();
 
   const handleMonthYear = (type: 'month' | 'year', value: string) => {
-    setMonthRange(prev => ({
+    const updatedMonth = type === 'month' ? String(padTo2Digits(i18n.monthNames[TranslationWidth.LONG]!.indexOf(value) + 1)) : range.month;
+    setRange(prev => ({
       ...prev,
-      [type]: type === 'month' ? String(padTo2Digits(i18n.monthNames[TranslationWidth.LONG]!.indexOf(value) + 1)) : value
+      [type]: type === 'month' ? updatedMonth : value
     }));
   };
 
@@ -28,60 +39,75 @@ export const useReportsDataHandling = () => {
     }));
   };
 
-  const { display, entityData, fetchData, fetchNextPage, fetchPrevPage, setEntityData }
-    = useEntityData({
-      category,
-      rangeType: lapse,
-      range: lapse === 'Día'
-        ? { day: dayRange.day, month: dayRange.month, year: dayRange.year }
-        : lapse === 'Mes'
-          ? { month: monthRange.month, year: monthRange.year }
-          : range
-    });
-
-  useEffect(() => {
-    if (lapse === 'Día') {
-      const sp = dayDateNumbersText.split('-');
-      setDayRange({ day: sp[0], month: sp[1], year: sp[2] });
+  const handleReportLapse = () => {
+    let updatedReportLapse = '';
+    switch (lapse) {
+      case 'Día':
+        updatedReportLapse = dayDateText;
+        break;
+      case 'Mes':
+        const selectedMonth = i18n.monthNames[TranslationWidth.LONG]?.at(Number(range.month) - 1);
+        updatedReportLapse = `${selectedMonth} de ${range.year}`;
+        break;
+      case 'Personalizado':
+        if (typeof range.startDate !== 'undefined' && typeof range.endDate !== 'undefined') {
+          const [dayStart, monthStart, yearStart] = range?.startDate!.split("-").map(Number);
+          const [dayEnd, monthEnd, yearEnd] = range?.endDate!.split("-").map(Number);
+          const formattedStartDate = formatDateString(new Date(yearStart, monthStart - 1, dayStart));
+          const formattedEndDate = formatDateString(new Date(yearEnd, monthEnd - 1, dayEnd));
+          updatedReportLapse = `${formattedStartDate} - ${formattedEndDate}`;
+        }
+        break;
     }
-  }, [dayDateNumbersText, lapse]);
-
-  useEffect(() => {
-    setEntityData(null);
-    setLapse('');
-    setDayRange({ day: '', month: '', year: '' });
-    setMonthRange({ month: '', year: '' });
-    setRange({ startDate: '', endDate: '' });
-  }, [category]);
+    setReportLapse(updatedReportLapse);
+  };
 
   const isButtonEnabled = () => {
-    if (category === 'Usuarios') return true;
+    const isRangeDefined = typeof range.day !== 'undefined' &&
+      typeof range.month !== 'undefined' &&
+      typeof range.year !== 'undefined' &&
+      typeof range.startDate !== 'undefined' &&
+      typeof range.endDate !== 'undefined';
 
-    if (!category || !lapse) return false;
+    if (!category || lapse.length === 0 || isRangeDefined)
+      return false;
 
     switch (lapse) {
       case 'Día':
-        return dayRange.day && dayRange.month && dayRange.year;
+        return typeof range.day !== 'undefined' && typeof range.month !== 'undefined' && typeof range.year !== 'undefined';
       case 'Mes':
-        return monthRange.month && monthRange.year;
+        return typeof range.month !== 'undefined' && typeof range.year !== 'undefined';
       case 'Personalizado':
-        return range.startDate && range.endDate;
-      default:
-        return false;
+        return typeof range.startDate !== 'undefined' && typeof range.endDate !== 'undefined';
     }
   };
+
+  useEffect(() => {
+    if (lapse === 'Día' && typeof dayDate !== 'undefined') {
+      const sp = dayDateNumbersText.split('-');
+      setRange({ day: sp[0], month: sp[1], year: sp[2] });
+    }
+  }, [dayDateNumbersText, lapse, dayDate]);
+
+  useEffect(() => {
+    setEntityData(null);
+    setRange({ day: undefined, month: undefined, year: undefined, startDate: undefined, endDate: undefined });
+  }, [category, lapse]);
+
+  useEffect(() => {
+    handleReportLapse();
+  }, [lapse, dayDateText, range]);
 
   return {
     category,
     dayDate,
     dayDateNumbersText,
     dayDateText,
-    dayRange,
     display,
     entityData,
     lapse,
-    monthRange,
     range,
+    reportLapse,
     handleMonthYear,
     handlePeriod,
     fetchData,
@@ -90,8 +116,6 @@ export const useReportsDataHandling = () => {
     isButtonEnabled,
     setCategory,
     setLapse,
-    setDayRange,
-    setMonthRange,
     setRange,
     setDayDate
   };
