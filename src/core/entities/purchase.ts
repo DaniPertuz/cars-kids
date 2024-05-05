a0271220844846002b79e97c63c1657748139ffd
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-05-05 16:37:07 -0500

    Add desk and payment fields

--- src/core/entities/purchase.ts
+++ src/core/entities/purchase.ts
@@ -1,11 +1,15 @@
+import { IPayment } from '../../infrastructure/interfaces';
+import { Desk } from './desk';
 import { Product } from './product';
 import { User } from './user';
 
 export interface Purchase {
   _id?:         string;
   price:        number;
+  payment:      IPayment
   product:      Product;
   purchaseDate: Date;
   quantity:     number;
+  desk:         Desk;
   user:         User;
 }
