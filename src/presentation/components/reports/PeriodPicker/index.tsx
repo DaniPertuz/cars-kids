46b6c44f5b6a2b62f7d87a0ace534ba926a19495
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-05-05 17:01:56 -0500

    Update initial values and reset data function

--- src/presentation/components/reports/PeriodPicker/index.tsx
+++ src/presentation/components/reports/PeriodPicker/index.tsx
@@ -10,23 +10,28 @@ import { globalStyles } from '../../../styles/global.styles';
 import { styles } from './styles';
 
 interface Props {
-  handleSelectedRange: (startDate: string, endDate: string) => void;
+  handleSelectedRange: (startDate: string | undefined, endDate: string | undefined) => void;
 }
 
 export const PeriodPicker = ({ handleSelectedRange }: Props) => {
-  const [range, setRange] = useState<{ startDate?: Date | null, endDate?: Date | null; }>({
-    startDate: null,
-    endDate: null
+  const [range, setRange] = useState<{ startDate?: Date | undefined, endDate?: Date | undefined; }>({
+    startDate: undefined,
+    endDate: undefined
   });
 
-  const { date: startDate, setDate: setStartDate, dateNumbersText: startDateNumbersText, dateText: startDateText } = useFormattedDate();
-  const { date: endDate, setDate: setEndDate, dateNumbersText: endDateNumbersText, dateText: endDateText } = useFormattedDate();
+  const { setDate: setStartDate, dateNumbersText: startDateNumbersText, dateText: startDateText } = useFormattedDate();
+  const { setDate: setEndDate, dateNumbersText: endDateNumbersText, dateText: endDateText } = useFormattedDate();
 
   const onSelectedRange = () => {
     if (range.startDate && range.endDate) {
       setStartDate(range.startDate);
       setEndDate(range.endDate);
     }
+  };
+
+  const onReset = () => {
+    setRange({ startDate: undefined, endDate: undefined });
+    handleSelectedRange(undefined, undefined);
   };
 
   useEffect(() => {
@@ -51,7 +56,7 @@ export const PeriodPicker = ({ handleSelectedRange }: Props) => {
           style={globalStyles.fullWidth}
         />
         :
-        <EditSelectedDate text={`${startDateText}\n${endDateText}`} onPress={() => setRange({})} />
+        <EditSelectedDate text={`${startDateText}\n${endDateText}`} onPress={onReset} />
       }
     </Layout>
   );
