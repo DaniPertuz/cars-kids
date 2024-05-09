fb0fe13fbd4ed656c2f506e9a99f0aed9c0f67dd
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-05-09 13:36:31 -0500

    Add iconName prop and set conditional rendering depending on user status

--- src/presentation/components/ui/DeleteButton/index.tsx
+++ src/presentation/components/ui/DeleteButton/index.tsx
@@ -3,26 +3,25 @@ import { TouchableOpacity } from 'react-native';
 import { Layout } from '@ui-kitten/components';
 import { CustomIcon, DeleteModal } from '../';
 import { Product, User, Vehicle } from '../../../../core/entities';
+import { IStatus } from '../../../../infrastructure/interfaces';
 import { globalColors } from '../../../theme/globalColors';
 import { styles } from './styles';
 
 interface Props {
+  iconName: string;
   iconSize: number;
   product?: Product;
   user?: User;
   vehicle?: Vehicle;
 }
 
-export const DeleteButton = ({ iconSize, product, user, vehicle }: Props) => {
+export const DeleteButton = ({ iconName, iconSize, product, user, vehicle }: Props) => {
   const [visible, setVisible] = useState(false);
 
   return (
-    <Layout style={styles.container}>
-      <TouchableOpacity
-        activeOpacity={1.0}
-        onPress={() => setVisible(true)}
-      >
-        <CustomIcon name='trash-outline' size={{ height: iconSize, width: iconSize }} fillColor={globalColors.white} />
+    <Layout style={[styles.container, { backgroundColor: user?.status === IStatus.Inactive ? globalColors.successLight : globalColors.primaryRed }]}>
+      <TouchableOpacity activeOpacity={1.0} onPress={() => setVisible(true)}>
+        <CustomIcon name={iconName} size={{ height: iconSize, width: iconSize }} fillColor={globalColors.white} />
       </TouchableOpacity>
       <DeleteModal visible={visible} setVisible={setVisible} product={product} vehicle={vehicle} user={user} />
     </Layout>
