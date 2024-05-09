cb8f4a8f5a96b66bbc2cf010e9841e8be1154fd1
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-05-09 13:58:55 -0500

    Remove unused state variable

--- src/presentation/hooks/useUsersData.ts
+++ src/presentation/hooks/useUsersData.ts
@@ -3,14 +3,12 @@ import * as UsersUseCases from '../../core/use-cases/users';
 import { Pagination, UsersResponse } from '../../infrastructure/interfaces';
 
 export const useUsersData = () => {
-  const [display, setDisplay] = useState(false);
   const [usersData, setUsersData] = useState<UsersResponse>();
   const [paginationState, setPaginationState] = useState<Pagination>({ page: 1, limit: 10 });
 
   const fetchData = async () => {
     const data = await UsersUseCases.getUsersUseCase(`users?page=${paginationState.page}&limit=${paginationState.limit}`);
     setUsersData(data.response);
-    setDisplay(true);
   };
 
   useEffect(() => {
@@ -34,7 +32,6 @@ export const useUsersData = () => {
     fetchNextPage,
     fetchPrevPage,
     setUsersData,
-    display,
     usersData
   };
 };
