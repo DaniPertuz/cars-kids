4bd5da8a21427c3b3b718fb3aa76c508c80a5c10
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-05-09 14:01:56 -0500

    Set conditional rendering for EditButton component and icon for DeleteButton component

--- src/presentation/components/reports/ReportActions/index.tsx
+++ src/presentation/components/reports/ReportActions/index.tsx
@@ -1,13 +1,14 @@
 import { Layout } from '@ui-kitten/components';
 import { DeleteButton, EditButton } from '../../ui';
 import { User } from '../../../../core/entities';
+import { IStatus } from '../../../../infrastructure/interfaces';
 import { styles } from './styles';
 
 export const ReportActions = ({ user }: { user: User; }) => {
   return (
     <Layout style={styles.container}>
-      <EditButton iconSize={25} user={user} />
-      <DeleteButton iconSize={25} user={user} />
+      {user.status === IStatus.Active && <EditButton iconSize={25} user={user} />}
+      <DeleteButton iconName={user.status === IStatus.Inactive ? 'person-done-outline' : 'person-delete-outline'} iconSize={25} user={user} />
     </Layout>
   );
 };
