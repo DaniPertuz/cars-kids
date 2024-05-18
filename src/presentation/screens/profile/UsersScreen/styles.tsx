8c581e738668dee09b3a6a6c13bd1b0c493b7d97
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-05-18 10:51:27 -0500

    Add screen styles

---
+++ src/presentation/screens/profile/UsersScreen/styles.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+import { globalColors } from '../../../theme/globalColors';
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: globalColors.background,
+    flex: 1,
+    gap: 10
+  }
+});
