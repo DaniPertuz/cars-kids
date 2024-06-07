1347ac7305defe73fcd609791a65d02bb54da1a9
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-06 21:28:45 -0500

    Set custom props for Spinner component

--- src/presentation/components/ui/LoadingIndicator/index.tsx
+++ src/presentation/components/ui/LoadingIndicator/index.tsx
@@ -2,10 +2,15 @@ import { View } from 'react-native';
 import { Spinner } from '@ui-kitten/components';
 import { globalStyles } from '../../../styles/global.styles';
 
-export const LoadingIndicator = () => {
+interface Props {
+  borderColor?: string;
+  size?: string;
+}
+
+export const LoadingIndicator = ({ borderColor, size }: Props) => {
   return (
     <View style={globalStyles.alignJustifyCenter}>
-      <Spinner size='tiny' style={globalStyles.whiteBorder} />
+      <Spinner size={size ? size : 'tiny'} style={borderColor ? { borderColor } : globalStyles.whiteBorder} />
     </View>
   );
 };
