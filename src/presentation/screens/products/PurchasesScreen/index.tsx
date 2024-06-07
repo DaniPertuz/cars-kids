a141147dfad48b00cf2fa95748d48d0fc179cd22
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-06 22:51:34 -0500

    Add modal component as param for MainScreenHeader

--- src/presentation/screens/products/PurchasesScreen/index.tsx
+++ src/presentation/screens/products/PurchasesScreen/index.tsx
@@ -1,10 +1,11 @@
 import { CustomDivider, MainLayout, MainScreenHeader } from '../../../components/ui';
 import { PurchasesListComponent } from '../../../components/purchases/PurchasesListComponent';
+import { PurchaseEntryModal } from '../../../components/purchases/PurchaseEntryModal';
 
 export const PurchasesScreen = () => {
   return (
     <MainLayout>
-      <MainScreenHeader />
+      <MainScreenHeader ModalComponent={PurchaseEntryModal} purchases />
       <CustomDivider />
       <PurchasesListComponent />
     </MainLayout>
