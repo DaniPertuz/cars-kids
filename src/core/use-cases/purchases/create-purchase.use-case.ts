3c4dfdb9535f572be192ec7f766b3920f34de43f
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-06 22:11:41 -0500

    Update object to create purchase

--- src/core/use-cases/purchases/create-purchase.use-case.ts
+++ src/core/use-cases/purchases/create-purchase.use-case.ts
@@ -1,12 +1,24 @@
 import { AxiosError } from 'axios';
 import carskidsApi from '../../../config/api/carskidsApi';
-import { PurchaseAPIResponse } from '../../../infrastructure/interfaces';
+import { IPayment, PurchaseAPIResponse } from '../../../infrastructure/interfaces';
 import { Purchase } from '../../entities';
 
 export const createPurchaseUseCase = async (purchase: Purchase): Promise<PurchaseAPIResponse> => {
   try {
-    const { data } = await carskidsApi.post<PurchaseAPIResponse>('purchases', purchase);
-    return { purchase: data.purchase };
+    const { desk, payment, price, purchaseDate, quantity, user } = purchase;
+    const purchaseObj = {
+      product: purchase.product._id,
+      quantity,
+      price,
+      payment: payment as IPayment,
+      purchaseDate,
+      user: user._id,
+      desk: desk._id
+    };
+
+    const { data } = await carskidsApi.post<PurchaseAPIResponse>('purchases', purchaseObj);
+
+    return { purchase: data as Purchase };
   } catch (error: any) {
     if (error instanceof AxiosError) {
       return error.response?.data;
