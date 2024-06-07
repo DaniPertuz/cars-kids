4efbf2cc1d3fd04b91ae02200ac8cf850ef1d005
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-06 22:49:45 -0500

    Custom MainScreenHeaderLoadingSpinner component to display loading status when uploading purchases

---
+++ src/presentation/components/ui/MainScreenHeaderLoadingSpinner/index.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+import { Layout } from '@ui-kitten/components';
+import { LoadingIndicator } from '../LoadingIndicator';
+import { globalColors } from '../../../theme/globalColors';
+import { globalStyles } from '../../../styles/global.styles';
+
+export const MainScreenHeaderLoadingSpinner = () => {
+  return (
+    <Layout style={styles.container}>
+      <LoadingIndicator borderColor={globalColors.primaryRed} size='medium' />
+    </Layout>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    ...globalStyles.mainBackground
+  }
+});
