790282dbdb905bc68a35ff0560a7706ecd672f2b
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-07 23:17:34 -0500

    Custom DeskEntryModal modal component to add or update desks

---
+++ src/presentation/components/desks/DeskEntryModal/index.tsx
@@ -0,0 +1,27 @@
+import { Card, Layout, Modal } from '@ui-kitten/components';
+import { Desk } from '../../../../core/entities';
+import { useDeskEntryModalData } from '../../../hooks';
+import { DefaultInput } from '../../forms';
+import { HeaderFive, PrimaryButton } from '../../ui';
+import { globalStyles } from '../../../styles/global.styles';
+
+interface Props {
+  desk?: Desk;
+  visible: boolean;
+  setVisible: (value: boolean) => void;
+}
+
+export const DeskEntryModal = ({ desk, visible, setVisible }: Props) => {
+  const { loading, deskState, handleFieldChange, onSubmit } = useDeskEntryModalData({ desk, visible, setVisible });
+  return (
+    <Modal visible={visible} backdropStyle={globalStyles.backdrop} onBackdropPress={() => setVisible(false)}>
+      <Card style={globalStyles.mainBackground}>
+        <Layout style={globalStyles.modalContainer}>
+          <HeaderFive text={`${desk ? 'Actualizar' : 'Nuevo'} puesto de trabajo`} />
+          <DefaultInput caption='Este valor es único' placeholder={'Nombre'} value={deskState.name} onChangeText={(name) => handleFieldChange('name', name)} />
+          <PrimaryButton disabled={loading} text={desk ? 'Actualizar' : 'Agregar'} onPress={onSubmit} />
+        </Layout>
+      </Card>
+    </Modal>
+  );
+};
