1945a33b1d9f3f0bd7e4c31e93cd4eea0db81089
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-08 15:00:24 -0500

    Update desks data handling

--- src/presentation/hooks/useDeskData.ts
+++ src/presentation/hooks/useDeskData.ts
@@ -1,21 +1,34 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDesksStore } from '../store/desk/useDeskStore';
+import * as DeskUseCases from '../../core/use-cases/desks';
+import { Desk } from '../../core/entities';
 
 export const useDeskData = () => {
   const selectedDesk = useDesksStore(state => state.selectedDesk);
-  const desks = useDesksStore(state => state.desks);
-  const desksData = useDesksStore(state => state.desksData);
-  const getDesks = useDesksStore(state => state.getDesks);
   const setSelectedDesk = useDesksStore(state => state.setSelectedDesk);
+  const [desks, setDesks] = useState<Desk[]>([]);
+  const [total, setTotal] = useState<number>(0);
 
-  const getAllDesks = () => {
-    const total = desksData.total;
-    getDesks(total);
+  const getDesksTotal = async () => {
+    const resp = await DeskUseCases.getDesksUseCase('desks');
+    const total = resp.response?.total || 0;
+    setTotal(total);
+  };
+
+  const getAllDesks = async (limit?: number) => {
+    const query = limit ? `desks?limit=${limit}` : 'desks';
+    const resp = await DeskUseCases.getDesksUseCase(query);
+    setDesks(resp.response?.desks || []);
+  };
+
+  const fetchData = async () => {
+    await getDesksTotal();
+    await getAllDesks(total);
   };
 
   useEffect(() => {
-    getAllDesks();
-  }, [desks]);
+    fetchData();
+  }, [total, desks]);
 
   return {
     desks,
