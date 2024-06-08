f03adff3e9aa0716062b74d558de61391733459d
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-07 23:14:36 -0500

    Update implementation to get desks data

--- src/presentation/hooks/useDeskData.ts
+++ src/presentation/hooks/useDeskData.ts
@@ -4,12 +4,18 @@ import { useDesksStore } from '../store/desk/useDeskStore';
 export const useDeskData = () => {
   const selectedDesk = useDesksStore(state => state.selectedDesk);
   const desks = useDesksStore(state => state.desks);
+  const desksData = useDesksStore(state => state.desksData);
   const getDesks = useDesksStore(state => state.getDesks);
   const setSelectedDesk = useDesksStore(state => state.setSelectedDesk);
 
+  const getAllDesks = () => {
+    const total = desksData.total;
+    getDesks(total);
+  };
+
   useEffect(() => {
-    getDesks();
-  }, []);
+    getAllDesks();
+  }, [desks]);
 
   return {
     desks,
