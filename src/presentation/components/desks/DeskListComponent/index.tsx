fe903a37d1d424f0d7df6d94e1e5b30c03a2ab0d
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-08 14:55:27 -0500

    Update style rules of component layout

--- src/presentation/components/desks/DeskListComponent/index.tsx
+++ src/presentation/components/desks/DeskListComponent/index.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 export const DeskListComponent = ({ bottom, desks }: Props) => {
   return (
-    <Layout style={{ ...globalStyles.mainBackground, marginHorizontal: 20, paddingBottom: bottom + 150 }}>
+    <Layout style={{ ...globalStyles.mainBackground, marginHorizontal: 20, paddingBottom: bottom + 325 }}>
       {(desks.length === 0)
           ?
           <EmptyListMessage heightBy={0.7} text='No hay puestos de trabajo' />
