08e6ab60f5b467f7dc917f9a9fb32777122719af
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-07 23:10:08 -0500

    Add variable to get desks data and update URL params to get desks list

--- src/presentation/store/desk/useDeskStore.ts
+++ src/presentation/store/desk/useDeskStore.ts
@@ -3,11 +3,13 @@ import { Desk } from '../../../core/entities';
 import { persist } from 'zustand/middleware';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as DeskUseCases from '../../../core/use-cases/desks';
+import { DeskResponse } from '../../../infrastructure/interfaces';
 
 interface DeskState {
   desks: Desk[];
+  desksData: DeskResponse;
   selectedDesk: Desk | undefined;
-  getDesks: () => void;
+  getDesks: (limit?: number) => void;
   setSelectedDesk: (desk: Desk) => void;
 }
 
@@ -15,10 +17,20 @@ export const useDesksStore = create<DeskState>()(
   persist(
     (set) => ({
       desks: [],
+      desksData: {
+        page: 0,
+        limit: 0,
+        total: 0,
+        next: null,
+        prev: null,
+        desks: []
+      },
       selectedDesk: undefined,
-      getDesks: async () => {
-        const resp = await DeskUseCases.getDesksUseCase('desks');
+      getDesks: async (limit?: number) => {
+        const query = limit ? `desks?limit=${limit}` : 'desks';
+        const resp = await DeskUseCases.getDesksUseCase(query);
         set({ desks: resp.response?.desks });
+        set({ desksData: resp.response });
       },
       setSelectedDesk: (desk: Desk) => {
         set({ selectedDesk: desk });
