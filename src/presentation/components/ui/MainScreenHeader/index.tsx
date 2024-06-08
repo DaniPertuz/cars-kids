73789d7c7149cf9e216fc69152a7b250e1063248
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-07 23:56:17 -0500

    Set custom text for title

--- src/presentation/components/ui/MainScreenHeader/index.tsx
+++ src/presentation/components/ui/MainScreenHeader/index.tsx
@@ -11,17 +11,18 @@ import { styles } from './styles';
 
 interface Props {
   purchases: boolean;
+  title: string;
   ModalComponent: React.ComponentType<{ visible: boolean; setVisible: (visible: boolean) => void; desk: Desk; }>;
 }
 
-export const MainScreenHeader = ({ purchases, ModalComponent }: Props) => {
+export const MainScreenHeader = ({ purchases, title, ModalComponent }: Props) => {
   const { top } = useSafeAreaInsets();
   const { desks, selectedDesk, loading, visible, handleDesk, handleUpload, setVisible, showPurchaseModal } = useMainScreenHeaderData({ purchases });
 
   return (
     <Layout style={{ marginTop: top, ...styles.container }}>
       <MainScreenHeaderButton iconName={'plus-circle'} onPress={showPurchaseModal} />
-      <MainScreenHeaderTitle desks={desks} selectedDesk={selectedDesk!} title='Compras' handleDesk={handleDesk} />
+      <MainScreenHeaderTitle desks={desks} selectedDesk={selectedDesk!} title={title} handleDesk={handleDesk} />
       {!loading
         ?
         <MainScreenHeaderButton iconName={'upload-outline'} onPress={handleUpload} />
