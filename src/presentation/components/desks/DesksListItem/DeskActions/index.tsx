3b02cd6f82fd9a1e33a2e9d494ad745c16894a28
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-08 14:31:47 -0500

    Format line

--- src/presentation/components/desks/DesksListItem/DeskActions/index.tsx
+++ src/presentation/components/desks/DesksListItem/DeskActions/index.tsx
@@ -3,7 +3,7 @@ import { Desk } from '../../../../../core/entities';
 import { EditButton, DeleteButton } from '../../../ui';
 import { styles } from '../styles';
 
-export const DeskActions = ({ desk }: { desk: Desk }) => {
+export const DeskActions = ({ desk }: { desk: Desk; }) => {
   return (
     <Layout style={styles.container}>
       <EditButton iconSize={25} desk={desk} />
