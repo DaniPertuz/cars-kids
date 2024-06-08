667c92e179045f60dcaa336250a8e041c1240b40
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-08 14:56:18 -0500

    Update return object type

--- src/core/use-cases/desks/delete-desk.use-case.ts
+++ src/core/use-cases/desks/delete-desk.use-case.ts
@@ -6,7 +6,7 @@ import { Desk } from '../../entities';
 export const deleteDeskUseCase = async (desk: Desk): Promise<DeskAPIResponse> => {
   try {
     const { data } = await carskidsApi.delete<DeskAPIResponse>(`desks/${desk.name}`);
-    return { desk: data.desk };
+    return { desk: data as Desk };
   } catch (error: any) {
     if (error instanceof AxiosError) {
       return error.response?.data;
