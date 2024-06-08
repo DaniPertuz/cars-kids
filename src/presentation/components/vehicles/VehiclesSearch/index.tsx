ff3088d6f3f2953d6adf05759957af759b1321f8
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-08 14:59:28 -0500

    Implementation of custom VehiclesSearchList component and useVehicleSearchData hook

--- src/presentation/components/vehicles/VehiclesSearch/index.tsx
+++ src/presentation/components/vehicles/VehiclesSearch/index.tsx
@@ -1,52 +1,16 @@
-import { useEffect, useMemo, useState } from 'react';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Layout, Spinner } from '@ui-kitten/components';
 
+import { useVehicleSearchData } from '../../../hooks';
 import { DefaultInput } from '../../forms';
 import { BackgroundImage, EmptyListMessage } from '../../ui';
-import { useDebouncedValue } from '../../../hooks';
-import { Vehicle } from '../../../../core/entities';
-import { VehiclesList } from '../VehiclesList';
-import * as VehicleUseCases from '../../../../core/use-cases/vehicles';
+import { VehiclesSearchList } from './VehiclesSearchList';
 
 import { globalStyles } from '../../../styles/global.styles';
 
 export const VehiclesSearch = () => {
   const { top } = useSafeAreaInsets();
-  const [search, setSearch] = useState('');
-  const [total, setTotal] = useState<number>(0);
-  const [vehiclesList, setVehiclesList] = useState<Vehicle[]>([]);
-  const [loading, setLoading] = useState(false);
-  const debouncedValue = useDebouncedValue(search);
-
-  const getTotalVehicles = async () => {
-    const resp = await VehicleUseCases.getVehiclesUseCase('vehicles');
-    setTotal(resp.response?.total || 0);
-  };
-
-  const getVehiclesData = async (limit: number) => {
-    const resp = await VehicleUseCases.getVehiclesUseCase(`vehicles?limit=${limit}`);
-    setVehiclesList(resp.response?.vehicles || []);
-  };
-
-  const fetchData = async () => {
-    setLoading(true);
-    await getTotalVehicles();
-    await getVehiclesData(total);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, [total]);
-
-  const vehicles = useMemo(() => {
-    if (debouncedValue.length < 3) return [];
-
-    return vehiclesList.filter(vehicle =>
-      vehicle.nickname.toLocaleLowerCase().includes(debouncedValue.toLocaleLowerCase()),
-    );
-  }, [debouncedValue, vehiclesList]);
+  const { debouncedValue, loading, search, vehicles, setSearch } = useVehicleSearchData();
 
   return (
     <Layout style={{ ...globalStyles.searchContainer, marginTop: top }}>
@@ -60,9 +24,7 @@ export const VehiclesSearch = () => {
       {(vehicles.length === 0 && debouncedValue.length > 2) &&
         <EmptyListMessage heightBy={0.7} text={`No hay vehículos con nombre/apodo "${debouncedValue}"`} />
       }
-      <Layout style={globalStyles.searchListContainer}>
-        <VehiclesList vehicles={vehicles} />
-      </Layout>
+      <VehiclesSearchList vehicles={vehicles} />
     </Layout>
   );
 };
