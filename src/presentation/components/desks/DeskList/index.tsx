beff70d9aad372e44020dccb00205448f1d56753
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-07 23:23:18 -0500

    Custom DeskList component for desks list

---
+++ src/presentation/components/desks/DeskList/index.tsx
@@ -0,0 +1,19 @@
+import { Layout, List } from '@ui-kitten/components';
+import { DesksListItem } from '../DesksListItem';
+import { Desk } from '../../../../core/entities';
+import { globalStyles } from '../../../styles/global.styles';
+
+export const DeskList = ({ desks }: { desks: Desk[]; }) => {
+  return (
+    <List
+      data={desks}
+      showsVerticalScrollIndicator={false}
+      style={globalStyles.mainBackground}
+      renderItem={({ item }) => (
+        <Layout style={globalStyles.mainBackground}>
+          <DesksListItem desk={item} />
+        </Layout>
+      )}
+    />
+  );
+};
