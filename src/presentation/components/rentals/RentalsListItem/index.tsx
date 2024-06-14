f48500d571471f2b24b726122751c02e78a9afcb
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-13 21:52:03 -0500

    Custom RentalsListItem component to display transactions list items

---
+++ src/presentation/components/rentals/RentalsListItem/index.tsx
@@ -0,0 +1,34 @@
+import { Layout, ListItem } from '@ui-kitten/components';
+import { Rental } from '../../../../core/entities';
+import { paymentDescriptions } from '../../../../utils';
+import { TransactionActions } from '../../transactions/TransactionActions';
+import { HeaderSix } from '../../ui';
+import { useFormattedDate } from '../../../hooks';
+
+interface Props {
+  rental: Rental;
+}
+
+export const RentalsListItem = ({ rental }: Props) => {
+  const { addedTime, extractTimeFromStringDate } = useFormattedDate();
+
+  const rentalDescription = () => {
+    const safeAccess = (value: any) => value || 'N/A';
+    const payment = paymentDescriptions[rental.payment];
+    let description = `Inicio: ${extractTimeFromStringDate(rental.date)}\nFin: ${addedTime(rental.date, rental.time)}\nVehículo: ${safeAccess(rental.vehicle?.nickname)}\nPago: ${safeAccess(rental.amount)}\nMedio: ${payment}\nPuesto de trabajo: ${rental.desk.name}\nUsuario: ${safeAccess(rental.user?.name)}`;
+    if (rental.exception) {
+      description += `\nObservación: ${rental.exception}`;
+    }
+    return description;
+  };
+
+  return (
+    <ListItem
+      title={rental.client}
+      description={rentalDescription()}
+      accessoryLeft={<Layout><HeaderSix text={`${rental.time}'`} /></Layout>}
+      accessoryRight={<TransactionActions rental={rental} />}
+      style={{ borderRadius: 10, margin: 10 }}
+    />
+  );
+};
