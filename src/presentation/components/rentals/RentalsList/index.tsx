0e510dad2ec8f21da2d9f189323d746aa860bd69
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-13 21:52:32 -0500

    Custom RentalsListI component to display transactions list

---
+++ src/presentation/components/rentals/RentalsList/index.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+import { List } from '@ui-kitten/components';
+import { Rental } from '../../../../core/entities';
+import { RentalsListItem } from '../RentalsListItem';
+import { globalStyles } from '../../../styles/global.styles';
+
+export const RentalsList = ({ rentals }: { rentals: Rental[]; }) => {
+  return (
+    <List
+      data={rentals}
+      showsVerticalScrollIndicator={false}
+      style={styles.container}
+      renderItem={({ item }) => <RentalsListItem rental={item} />}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    ...globalStyles.mainBackground,
+    marginVertical: 5
+  }
+});
