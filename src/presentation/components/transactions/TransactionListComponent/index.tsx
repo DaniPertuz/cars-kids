de73d7579de778b58ccf9bf8743cf3d12d59503c
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-14 23:17:27 -0500

    Implement custom useTransactionStore and update conditional rendering with custom lists rendering components

--- src/presentation/components/transactions/TransactionListComponent/index.tsx
+++ src/presentation/components/transactions/TransactionListComponent/index.tsx
@@ -1,11 +1,9 @@
 import { StyleSheet } from 'react-native';
 import { Layout } from '@ui-kitten/components';
-import { IPayment, IStatus, IUserRole, IVehicleCategory, IVehicleSize, Transaction } from '../../../../infrastructure/interfaces';
-import { usePurchasesStore } from '../../../store/purchases/usePurchasesStore';
-import { PurchasesList } from '../../purchases/PurchasesList';
-import { EmptyListMessage } from '../../ui';
-import { Rental } from '../../../../core/entities';
-import { RentalsList } from '../../rentals/RentalsList';
+import { Transaction } from '../../../../infrastructure/interfaces';
+import { useTransactionStore } from '../../../store/transactions/useTransactionsStore';
+import { PurchasesListContent } from '../../purchases/PurchasesListContent';
+import { RentalsListContent } from '../../rentals/RentalsListContent';
 import { globalStyles } from '../../../styles/global.styles';
 
 interface Props {
@@ -13,52 +11,12 @@ interface Props {
 }
 
 export const TransactionsListComponent = ({ entity }: Props) => {
-  const purchases = usePurchasesStore(state => state.purchases);
-  const rentals: Rental[] = [{
-    client: '',
-    time: 15,
-    date: new Date(),
-    vehicle: {
-      nickname: 'Vehículo 1',
-      category: IVehicleCategory.Car,
-      color: '#1DCFA1',
-      size: IVehicleSize.Large
-    },
-    payment: IPayment.Cash,
-    amount: 10000,
-    desk: {
-      name: 'Puesto 1'
-    },
-    user: {
-      email: 'usuario@test.com',
-      name: 'Usuario Test',
-      role: IUserRole.Admin,
-      status: IStatus.Active
-    }
-  }]
+  const purchases = useTransactionStore(state => state.purchases);
+  const rentals = useTransactionStore(state => state.rentals);
 
   return (
     <Layout style={styles.container}>
-      {entity === 'Purchase'
-        ?
-        <>
-          {purchases.length === 0
-            ?
-            <EmptyListMessage heightBy={0.75} text='No hay compras' />
-            :
-            <PurchasesList purchases={purchases} />
-          }
-        </>
-        :
-        <>
-          {rentals.length === 0
-            ?
-            <EmptyListMessage heightBy={0.75} text='No hay alquileres' />
-            :
-            <RentalsList rentals={rentals} />
-          }
-        </>
-      }
+      {entity === 'Purchase' ? <PurchasesListContent purchases={purchases} /> : <RentalsListContent rentals={rentals} />}
     </Layout>
   );
 };
