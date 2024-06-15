ae0714c9ed1b02827cd95eda6de63e140ae68d62
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-14 23:58:59 -0500

    Set conditional text value

--- src/presentation/components/ui/ModalTitle/index.tsx
+++ src/presentation/components/ui/ModalTitle/index.tsx
@@ -1,14 +1,15 @@
 import { StyleSheet } from 'react-native';
 import { Layout } from '@ui-kitten/components';
-import { Purchase } from '../../../../core/entities';
+import { Purchase, Rental } from '../../../../core/entities';
+import { Transaction } from '../../../../infrastructure/interfaces';
 import { HeaderSix, CustomIcon } from '..';
 import { globalStyles } from '../../../styles/global.styles';
 import { globalColors } from '../../../theme/globalColors';
 
-export const ModalTitle = ({ purchase }: { purchase?: Purchase; }) => {
+export const ModalTitle = ({ purchase, rental, transaction }: { purchase?: Purchase, rental?: Rental, transaction: Transaction; }) => {
   return (
     <Layout style={styles.container}>
-      <HeaderSix text={`${purchase ? 'Editar' : 'Nueva'} compra`} />
+      <HeaderSix text={`${(purchase || rental) ? 'Editar' : 'Agregar'} ${transaction === 'Purchase' ? 'compra' : 'alquiler'}`} />
       <CustomIcon name='shopping-cart-outline' size={{ height: 35, width: 35 }} fillColor={globalColors.secondaryRed} />
     </Layout>
   );
