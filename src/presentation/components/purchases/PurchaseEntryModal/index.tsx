8d55687156aa5c8ec5be238a702cafb50cb1e391
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-19 23:11:46 -0500

    Replace hook to be used and update conditional rendering inside modal

--- src/presentation/components/purchases/PurchaseEntryModal/index.tsx
+++ src/presentation/components/purchases/PurchaseEntryModal/index.tsx
@@ -2,9 +2,9 @@ import { useMemo } from 'react';
 import { Card, Input, Layout, Modal } from '@ui-kitten/components';
 
 import { Desk, Purchase } from '../../../../core/entities';
-import { usePurchaseEntryModalData } from '../../../hooks';
-import { ModalTitle, PrimaryButton, SelectComponent } from '../../ui';
-import { PurchaseEntryModalPaymentFeesSelects } from '../PurchaseEntryModalPaymentFeesSelects';
+import { useTransactionEntryModalData } from '../../../hooks';
+import { LoadingIndicator, ModalTitle, PrimaryButton, SelectComponent } from '../../ui';
+import { TransactionEntryModalPaymentFeesSelects } from '../../transactions/TransactionEntryModalPaymentFeesSelects';
 
 import { globalStyles } from '../../../styles/global.styles';
 
@@ -36,36 +36,41 @@ export const PurchaseEntryModal = ({ desk, purchase, visible, setVisible }: Prop
     setSecondFee,
     setThirdFee,
     onSubmit
-  } = usePurchaseEntryModalData({ desk, purchase, visible, setVisible });
+  } = useTransactionEntryModalData({ desk, purchase, visible, setVisible });
 
   const productOptions = useMemo(() => products.map(p => p.name), [products]);
 
   return (
     <Modal visible={visible} backdropStyle={globalStyles.backdrop} onBackdropPress={() => setVisible(false)}>
-      <Card style={globalStyles.mainBackground}>
-        <Layout style={globalStyles.transactionsContainer}>
-          <Layout style={globalStyles.transactionsOptionsContainer}>
-            <ModalTitle purchase={purchase} transaction='Purchase' />
-            <SelectComponent placeholder={'Producto'} initialValue={purchase ? purchase.product.name : ''} options={productOptions} handleSelection={handleProduct} />
-            {(newPurchase.product.price || purchase?.product.price) && <Input label={'Precio'} disabled value={String(purchase ? purchase.product.price : newPurchase.product.price)} />}
-            <SelectComponent placeholder={'Cantidad'} initialValue={purchase ? String(purchase.quantity) : ''} options={quantityPurchases()} handleSelection={handleQuantity} />
-            <SelectComponent placeholder={'Medio de pago'} initialValue={purchase ? convertPurchasePayment(purchase.payment) : ''} options={paymentOptions()} handleSelection={handlePayment} />
-            {customPayment &&
-              <PurchaseEntryModalPaymentFeesSelects
-                firstFee={firstFee}
-                secondFee={secondFee}
-                thirdFee={thirdFee}
-                handleFeePayment={handleFeePayment}
-                handleFeeValue={handleFeeValue}
-                setFirstFee={setFirstFee}
-                setSecondFee={setSecondFee}
-                setThirdFee={setThirdFee}
-              />
-            }
+      {loading
+        ?
+        <LoadingIndicator />
+        :
+        <Card style={globalStyles.mainBackground}>
+          <Layout style={globalStyles.transactionsContainer}>
+            <Layout style={globalStyles.transactionsOptionsContainer}>
+              <ModalTitle purchase={purchase} transaction='Purchase' setVisible={() => setVisible(false)} />
+              <SelectComponent placeholder={'Producto'} initialValue={purchase ? purchase.product.name : ''} options={productOptions} handleSelection={handleProduct} />
+              {(newPurchase.product.price || purchase?.product.price) && <Input label={'Precio'} disabled value={String(purchase ? purchase.product.price : newPurchase.product.price)} />}
+              <SelectComponent placeholder={'Cantidad'} initialValue={purchase ? String(purchase.quantity) : ''} options={quantityPurchases()} handleSelection={(value: string) => handleQuantity(value, "Purchase")} />
+              <SelectComponent placeholder={'Medio de pago'} initialValue={purchase ? convertPurchasePayment(purchase.payment) : ''} options={paymentOptions()} handleSelection={(value: string) => handlePayment(value, "Purchase")} />
+              {customPayment &&
+                <TransactionEntryModalPaymentFeesSelects
+                  firstFee={firstFee}
+                  secondFee={secondFee}
+                  thirdFee={thirdFee}
+                  handleFeePayment={handleFeePayment}
+                  handleFeeValue={handleFeeValue}
+                  setFirstFee={setFirstFee}
+                  setSecondFee={setSecondFee}
+                  setThirdFee={setThirdFee}
+                />
+              }
+            </Layout>
+            <PrimaryButton disabled={loading} text={purchase ? 'Actualizar' : 'Agregar'} onPress={() => onSubmit('Purchase')} />
           </Layout>
-          <PrimaryButton disabled={loading} text={purchase ? 'Actualizar' : 'Agregar'} onPress={onSubmit} />
-        </Layout>
-      </Card>
+        </Card>
+      }
     </Modal>
   );
 };
