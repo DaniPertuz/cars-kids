e3d25771e151f24ec23162f7c5dd6106f7c1e147
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-19 23:12:26 -0500

    Update modal component to be displayed

--- src/presentation/screens/rentals/RentalsScreen/index.tsx
+++ src/presentation/screens/rentals/RentalsScreen/index.tsx
@@ -1,11 +1,11 @@
 import { CustomDivider, MainLayout, MainScreenHeader } from '../../../components/ui';
-import { PurchaseEntryModal } from '../../../components/purchases/PurchaseEntryModal';
+import { RentalEntryModal } from '../../../components/rentals/RentalEntryModal';
 import { TransactionsListComponent } from '../../../components/transactions/TransactionListComponent';
 
 export const RentalsScreen = () => {
   return (
     <MainLayout>
-      <MainScreenHeader title='Alquileres' transaction={'Rental'} ModalComponent={PurchaseEntryModal} />
+      <MainScreenHeader title='Alquileres' transaction={'Rental'} ModalComponent={RentalEntryModal} />
       <CustomDivider />
       <TransactionsListComponent entity='Rental' />
     </MainLayout>
