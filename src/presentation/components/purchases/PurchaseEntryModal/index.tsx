75fd6cee0fa233202c6027a8a149e8ffb1f35df1
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-20 00:14:29 -0500

    Implementation of ModalBody component

--- src/presentation/components/purchases/PurchaseEntryModal/index.tsx
+++ src/presentation/components/purchases/PurchaseEntryModal/index.tsx
@@ -1,11 +1,6 @@
-import { useMemo } from 'react';
-import { Card, Input, Layout, Modal } from '@ui-kitten/components';
-
+import { Modal } from '@ui-kitten/components';
 import { Desk, Purchase } from '../../../../core/entities';
-import { useTransactionEntryModalData } from '../../../hooks';
-import { LoadingIndicator, ModalTitle, PrimaryButton, SelectComponent } from '../../ui';
-import { TransactionEntryModalPaymentFeesSelects } from '../../transactions/TransactionEntryModalPaymentFeesSelects';
-
+import { ModalBody } from '../../ui';
 import { globalStyles } from '../../../styles/global.styles';
 
 interface Props {
@@ -16,61 +11,9 @@ interface Props {
 }
 
 export const PurchaseEntryModal = ({ desk, purchase, visible, setVisible }: Props) => {
-  const {
-    customPayment,
-    firstFee,
-    loading,
-    newPurchase,
-    products,
-    secondFee,
-    thirdFee,
-    convertPurchasePayment,
-    handleFeePayment,
-    handleFeeValue,
-    handlePayment,
-    handleProduct,
-    handleQuantity,
-    paymentOptions,
-    quantityPurchases,
-    setFirstFee,
-    setSecondFee,
-    setThirdFee,
-    onSubmit
-  } = useTransactionEntryModalData({ desk, purchase, visible, setVisible });
-
-  const productOptions = useMemo(() => products.map(p => p.name), [products]);
-
   return (
     <Modal visible={visible} backdropStyle={globalStyles.backdrop} onBackdropPress={() => setVisible(false)}>
-      {loading
-        ?
-        <LoadingIndicator />
-        :
-        <Card style={globalStyles.mainBackground}>
-          <Layout style={globalStyles.transactionsContainer}>
-            <Layout style={globalStyles.transactionsOptionsContainer}>
-              <ModalTitle purchase={purchase} transaction='Purchase' setVisible={() => setVisible(false)} />
-              <SelectComponent placeholder={'Producto'} initialValue={purchase ? purchase.product.name : ''} options={productOptions} handleSelection={handleProduct} />
-              {(newPurchase.product.price || purchase?.product.price) && <Input label={'Precio'} disabled value={String(purchase ? purchase.product.price : newPurchase.product.price)} />}
-              <SelectComponent placeholder={'Cantidad'} initialValue={purchase ? String(purchase.quantity) : ''} options={quantityPurchases()} handleSelection={(value: string) => handleQuantity(value, "Purchase")} />
-              <SelectComponent placeholder={'Medio de pago'} initialValue={purchase ? convertPurchasePayment(purchase.payment) : ''} options={paymentOptions()} handleSelection={(value: string) => handlePayment(value, "Purchase")} />
-              {customPayment &&
-                <TransactionEntryModalPaymentFeesSelects
-                  firstFee={firstFee}
-                  secondFee={secondFee}
-                  thirdFee={thirdFee}
-                  handleFeePayment={handleFeePayment}
-                  handleFeeValue={handleFeeValue}
-                  setFirstFee={setFirstFee}
-                  setSecondFee={setSecondFee}
-                  setThirdFee={setThirdFee}
-                />
-              }
-            </Layout>
-            <PrimaryButton disabled={loading} text={purchase ? 'Actualizar' : 'Agregar'} onPress={() => onSubmit('Purchase')} />
-          </Layout>
-        </Card>
-      }
+      <ModalBody desk={desk} purchase={purchase} transaction={'Purchase'} visible={false} setVisible={setVisible} />
     </Modal>
   );
 };
--- src/presentation/components/rentals/RentalEntryModal/index.tsx
+++ src/presentation/components/rentals/RentalEntryModal/index.tsx
@@ -1,12 +1,6 @@
-import { Card, Layout, Modal } from '@ui-kitten/components';
-
+import { Modal } from '@ui-kitten/components';
 import { Desk, Rental } from '../../../../core/entities';
-import { useTransactionEntryModalData } from '../../../hooks';
-import { DefaultInput } from '../../forms';
-import { TransactionEntryModalPaymentFeesSelects } from '../../transactions/TransactionEntryModalPaymentFeesSelects';
-import { LoadingIndicator, ModalTitle, PrimaryButton, SelectComponent } from '../../ui';
-import { VehiclesSelectComponent } from '../../vehicles/VehiclesSelectComponent';
-
+import { ModalBody } from '../../ui';
 import { globalStyles } from '../../../styles/global.styles';
 
 interface Props {
@@ -16,59 +10,10 @@ interface Props {
   setVisible: (visible: boolean) => void;
 }
 
-export const RentalEntryModal = ({ desk, rental, visible, setVisible }: Props) => {  
-  const {
-    customPayment,
-    firstFee,
-    loading,
-    newRental,
-    secondFee,
-    thirdFee,
-    vehicles,
-    setFirstFee,
-    setSecondFee,
-    setThirdFee,
-    handleFeePayment,
-    handleFeeValue,
-    handlePayment,
-    handleRentalClient,
-    handleRentalTime,
-    handleRentalVehicle,
-    paymentOptions,
-    onSubmit
-  } = useTransactionEntryModalData({ desk, visible, setVisible });
-
+export const RentalEntryModal = ({ desk, rental, visible, setVisible }: Props) => {
   return (
     <Modal visible={visible} backdropStyle={globalStyles.backdrop} onBackdropPress={() => setVisible(false)}>
-      {loading
-        ?
-        <LoadingIndicator />
-        :
-        <Card style={globalStyles.mainBackground}>
-          <Layout style={globalStyles.transactionsContainer}>
-            <Layout style={globalStyles.transactionsOptionsContainer}>
-              <ModalTitle rental={rental} transaction='Rental' setVisible={() => setVisible(false)} />
-              <DefaultInput placeholder={'Cliente'} value={newRental.client} onChangeText={(client: string) => handleRentalClient(client)} />
-              <SelectComponent placeholder={'Tiempo'} initialValue={''} options={['15', '20', '30']} handleSelection={handleRentalTime} />
-              <VehiclesSelectComponent placeholder={'Vehículo'} initialValue={''} vehicles={vehicles} handleSelection={handleRentalVehicle} />
-              <SelectComponent placeholder={'Medio de pago'} initialValue={''} options={paymentOptions()} handleSelection={(value: string) => handlePayment(value, "Rental")} />
-              {customPayment &&
-                <TransactionEntryModalPaymentFeesSelects
-                  firstFee={firstFee}
-                  secondFee={secondFee}
-                  thirdFee={thirdFee}
-                  handleFeePayment={handleFeePayment}
-                  handleFeeValue={handleFeeValue}
-                  setFirstFee={setFirstFee}
-                  setSecondFee={setSecondFee}
-                  setThirdFee={setThirdFee}
-                />
-              }
-            </Layout>
-            <PrimaryButton disabled={false} text={rental ? 'Actualizar' : 'Agregar'} onPress={() => onSubmit('Rental')} />
-          </Layout>
-        </Card>
-      }
+      <ModalBody desk={desk} rental={rental} transaction={'Rental'} visible={false} setVisible={setVisible} />
     </Modal>
   );
 };
