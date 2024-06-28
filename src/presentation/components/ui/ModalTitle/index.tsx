5a75bdad275fb9051f107b4970a8897f18b0a973
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-28 01:07:48 -0500

    Implement custom ModalCloseButton component and new styles rules

--- src/presentation/components/ui/ModalTitle/index.tsx
+++ src/presentation/components/ui/ModalTitle/index.tsx
@@ -1,34 +1,29 @@
-import { StyleSheet, TouchableOpacity } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { Layout } from '@ui-kitten/components';
 import { Purchase, Rental } from '../../../../core/entities';
 import { Transaction } from '../../../../infrastructure/interfaces';
-import { HeaderSix, CustomIcon } from '..';
+import { HeaderSix, ModalCloseButton } from '..';
 import { globalStyles } from '../../../styles/global.styles';
-import { globalColors } from '../../../theme/globalColors';
 
 interface Props {
   purchase?: Purchase;
   rental?: Rental;
-  transaction: Transaction;
+  transaction?: Transaction;
   setVisible: () => void;
 }
 
 export const ModalTitle = ({ purchase, rental, transaction, setVisible }: Props) => {
   return (
     <Layout style={styles.container}>
-      <HeaderSix text={`${(purchase || rental) ? 'Editar' : 'Agregar'} ${transaction === 'Purchase' ? 'compra' : 'alquiler'}`} />
-      <TouchableOpacity activeOpacity={1.0} onPress={setVisible}>
-        <CustomIcon name={'close-circle-outline'} size={{ height: 35, width: 35 }} fillColor={globalColors.secondaryRed} />
-      </TouchableOpacity>
+      <HeaderSix text={`${transaction ? (purchase || rental ? 'Editar' : 'Agregar') : 'Agregar'} ${transaction ? (transaction === 'Purchase' ? 'compra' : 'alquiler') : 'puesto de trabajo'}`} />
+      <ModalCloseButton onPress={setVisible} />
     </Layout>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    ...globalStyles.alignCenterRowSpaceBetween,
     ...globalStyles.mainBackground
   }
 });
