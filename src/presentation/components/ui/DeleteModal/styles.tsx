df3b38b0a52f58bffc6bc5cb375f80a5dc999342
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-29 18:56:13 -0500

    Update style rules

--- src/presentation/components/ui/DeleteModal/styles.tsx
+++ src/presentation/components/ui/DeleteModal/styles.tsx
@@ -5,10 +5,25 @@ export const styles = StyleSheet.create({
   backdrop: {
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
+  closeButtonContainer: {
+    alignItems: 'center',
+    flex: 1,
+    ...globalStyles.mainBackground
+  },
   container: {
     alignItems: 'center',
     gap: 10,
     width: 320,
     ...globalStyles.mainBackground
+  },
+  textContainer: {
+    alignItems: 'center',
+    flex: 100,
+    ...globalStyles.mainBackground
+  },
+  titleContainer: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    ...globalStyles.mainBackground
   }
-})
+});
