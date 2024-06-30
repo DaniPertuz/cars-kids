e574d9bc852751cc152df45588540781fa185ea9
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-29 21:09:02 -0500

    Set initial opacity and size values

--- src/presentation/components/rentals/RentalTimerButtons/index.tsx
+++ src/presentation/components/rentals/RentalTimerButtons/index.tsx
@@ -4,12 +4,15 @@ import { HeaderFive, BasicButton } from '../../ui';
 import { globalColors } from '../../../theme/globalColors';
 
 export const RentalTimerButtons = () => {
+  const buttonSize = { height: 25, width: 25 };
+  const buttonOpacity = 0.7;
+
   return (
     <Layout style={styles.container}>
       <HeaderFive text={`'`} />
-      <BasicButton activeOpacity={0.7} fillColor={globalColors.warning} size={{ height: 25, width: 25 }} iconName='pause-circle-outline' onPress={() => console.log('Pausa')} />
-      <BasicButton activeOpacity={0.7} fillColor={globalColors.darkDisabled} size={{ height: 25, width: 25 }} iconName='refresh-outline' onPress={() => console.log('Reanuda')} />
-      <BasicButton activeOpacity={0.7} fillColor={globalColors.successLight} size={{ height: 25, width: 25 }} iconName='checkmark-circle-2-outline' onPress={() => console.log('Termina')} />
+      <BasicButton activeOpacity={buttonOpacity} fillColor={globalColors.warning} size={buttonSize} iconName='pause-circle-outline' onPress={() => console.log('Pausa')} />
+      <BasicButton activeOpacity={buttonOpacity} fillColor={globalColors.darkDisabled} size={buttonSize} iconName='refresh-outline' onPress={() => console.log('Reanuda')} />
+      <BasicButton activeOpacity={buttonOpacity} fillColor={globalColors.successLight} size={buttonSize} iconName='checkmark-circle-2-outline' onPress={() => console.log('Termina')} />
     </Layout>
   );
 };
