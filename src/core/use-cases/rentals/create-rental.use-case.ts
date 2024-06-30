76b216df3b4dbfea7dbaf0d555ddff1ab2843f8f
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-30 10:38:25 -0500

    Set object to be uploaded

--- src/core/use-cases/rentals/create-rental.use-case.ts
+++ src/core/use-cases/rentals/create-rental.use-case.ts
@@ -5,8 +5,16 @@ import { RentalAPIResponse } from '../../../infrastructure/interfaces';
 
 export const createRentalUseCase = async (rental: Rental): Promise<RentalAPIResponse> => {
   try {
-    const { data } = await carskidsApi.post<RentalAPIResponse>('rentals', rental);
-    return { rental: data.rental };
+    const { desk, user, vehicle } = rental;
+    const rentalObj = {
+      ...rental,
+      desk: desk._id,
+      user: user._id,
+      vehicle: vehicle._id
+    };
+
+    const { data } = await carskidsApi.post<RentalAPIResponse>('rentals', rentalObj);
+    return { rental: data as Rental };
   } catch (error: any) {
     if (error instanceof AxiosError) {
       return error.response?.data;
