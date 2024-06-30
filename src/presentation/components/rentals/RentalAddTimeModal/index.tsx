ddad1dd06d0ce040114b5d19f1ab2c9f5ab664c4
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-30 01:31:17 -0500

    Custom RentalAddTimeModal modal to add minutes to current rentals

---
+++ src/presentation/components/rentals/RentalAddTimeModal/index.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { Card, Layout, Modal } from '@ui-kitten/components';
+import { Rental } from '../../../../core/entities';
+import { useTransactionStore } from '../../../store/transactions/useTransactionsStore';
+import { DefaultInput, NumericInput } from '../../forms';
+import { HeaderFive, PrimaryButton } from '../../ui';
+import { globalStyles } from '../../../styles/global.styles';
+
+interface Props {
+  index: number;
+  rental: Rental;
+  visible: boolean;
+  advanceTime: (timeToAdd: number) => void;
+  setVisible: (value: boolean) => void;
+}
+
+export const RentalAddTimeModal = ({ index, rental, visible, advanceTime, setVisible }: Props) => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [minutes, setMinutes] = useState<number>(0);
+  const [exception, setException] = useState<string>('');
+  const updateRental = useTransactionStore(state => state.updateTransaction);
+
+  const onSubmit = () => {
+    setLoading(true);
+    advanceTime((minutes * 60) * -1);
+    updateRental(index, { ...rental, exception }, 'Rental');
+    setLoading(false);
+    setVisible(false);
+    setMinutes(0);
+  };
+
+  return (
+    <Modal visible={visible} backdropStyle={globalStyles.backdrop} onBackdropPress={() => setVisible(false)}>
+      <Card style={globalStyles.mainBackground}>
+        <Layout style={globalStyles.modalContainer}>
+          <HeaderFive text='Agregar minutos' />
+          <NumericInput placeholder='Minutos adicionales' value={minutes} onChangeText={setMinutes} />
+          <DefaultInput placeholder='Observación' value={exception} onChangeText={setException} />
+          <PrimaryButton disabled={loading} text={'Agregar'} onPress={onSubmit} />
+        </Layout>
+      </Card>
+    </Modal>
+  );
+};
