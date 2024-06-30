56ff97c53873604352643212b2d99bfef96ed88d
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-06-30 12:52:24 -0500

    Update API URL address

--- src/config/api/carskidsApi.ts
+++ src/config/api/carskidsApi.ts
@@ -1,17 +1,8 @@
-import { Platform } from 'react-native';
 import axios from 'axios';
 import { StorageAdapter } from '../adapters/storage-adapter';
-import { API_URL as PROD_URL, API_URL_ANDROID, API_URL_IOS, STAGE } from '@env';
-
-export const API_URL =
-  (STAGE === 'prod')
-    ? PROD_URL
-    : Platform.OS === 'ios'
-      ? API_URL_IOS
-      : API_URL_ANDROID;
 
 const carskidsApi = axios.create({
-  baseURL: API_URL,
+  baseURL: 'https://cars-kids-be.onrender.com/api/',
   headers: {
     'Content-Type': 'application/json'
   }
