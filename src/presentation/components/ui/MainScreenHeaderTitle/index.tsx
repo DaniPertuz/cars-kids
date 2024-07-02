10711332255e851090befad390dd86b4dd89afa0
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-07-01 23:18:17 -0500

    Set conditional rendering for desks select

--- src/presentation/components/ui/MainScreenHeaderTitle/index.tsx
+++ src/presentation/components/ui/MainScreenHeaderTitle/index.tsx
@@ -1,16 +1,17 @@
 import { StyleSheet } from 'react-native';
-import { Layout } from '@ui-kitten/components';
+import { Layout, Spinner } from '@ui-kitten/components';
 
 import { Desk } from '../../../../core/entities';
 import { DeskSelect } from '../../desks/DeskSelect';
 import { HeaderFive } from '../HeaderFive';
 import { globalStyles } from '../../../styles/global.styles';
+import { globalColors } from '../../../theme/globalColors';
 
 interface Props {
   desks: Desk[];
   selectedDesk: Desk;
   title: string;
-  handleDesk: (value: string) => void
+  handleDesk: (value: string) => void;
 }
 
 export const MainScreenHeaderTitle = ({ desks, selectedDesk, title, handleDesk }: Props) => {
@@ -20,7 +21,14 @@ export const MainScreenHeaderTitle = ({ desks, selectedDesk, title, handleDesk }
   return (
     <Layout style={styles.container}>
       <HeaderFive text={title} />
-      <DeskSelect desksNames={desksNames} selectedDesk={selectedDesk} handleDesk={handleDesk} />
+      {desks.length === 0
+        ?
+        <Layout style={styles.loadingContainer}>
+          <Spinner size='small' style={{ borderColor: globalColors.primaryRed }} />
+        </Layout>
+        :
+        <DeskSelect desksNames={desksNames} selectedDesk={selectedDesk} handleDesk={handleDesk} />
+      }
     </Layout>
   );
 };
@@ -32,6 +40,11 @@ const styles = StyleSheet.create({
     gap: 10,
     justifyContent: 'space-between',
     marginBottom: 15,
+    ...globalStyles.mainBackground
+  },
+  loadingContainer: {
+    alignItems: 'center',
+    flex: 1,
     ...globalStyles.mainBackground
   }
 });
