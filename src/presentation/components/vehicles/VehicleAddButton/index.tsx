0c42e6fd969082f7553cf0658de73dfa36713f7e
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-07-13 16:56:47 -0500

    Implement custom useCustomTheme hook

--- src/presentation/components/vehicles/VehicleAddButton/index.tsx
+++ src/presentation/components/vehicles/VehicleAddButton/index.tsx
@@ -1,17 +1,15 @@
 import { useState } from 'react';
 import { Layout } from '@ui-kitten/components';
+import { useCustomTheme } from '../../../hooks';
 import { FAB } from '../../ui';
 import { VehicleEntryModal } from '../VehicleEntryModal';
 import { styles } from './styles';
 
-interface Props {
-  top: number;
-}
-
-export const VehicleAddButton = ({ top }: Props) => {
+export const VehicleAddButton = () => {
   const [visible, setVisible] = useState(false);
+  const { background } = useCustomTheme();
   return (
-    <Layout style={{ marginTop: top, ...styles.container }}>
+    <Layout style={[styles.container, background]}>
       <FAB iconName='plus-circle-outline' iconSize={50} onPress={() => setVisible(true)} />
       <VehicleEntryModal visible={visible} setVisible={setVisible} />
     </Layout>
