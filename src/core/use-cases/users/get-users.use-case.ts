9b1448e2e40a5f9ffb7701363a7a0fbf844efd17
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-07-13 16:40:05 -0500

    Add access token to request

--- src/core/use-cases/users/get-users.use-case.ts
+++ src/core/use-cases/users/get-users.use-case.ts
@@ -1,10 +1,16 @@
 import { AxiosError } from 'axios';
 import carskidsApi from '../../../config/api/carskidsApi';
 import { UserAPIResponse, UsersResponse } from '../../../infrastructure/interfaces';
+import { useAuthStore } from '../../../presentation/store/auth/useAuthStore';
 
 export const getUsersUseCase = async (url: string): Promise<UserAPIResponse> => {
+  const { token } = useAuthStore.getState();
   try {
-    const { data } = await carskidsApi.get<UsersResponse>(url);
+    const { data } = await carskidsApi.get<UsersResponse>(url, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
     return { response: data };
   } catch (error: any) {
     if (error instanceof AxiosError) {
