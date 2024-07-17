780dff7bfb3b49652ad1510d708f8c04f8e3db4c
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-07-17 01:47:08 -0500

    Add token prop

--- src/core/entities/user.ts
+++ src/core/entities/user.ts
@@ -3,6 +3,7 @@ import { IStatus, IUserRole } from '../../infrastructure/interfaces';
 export interface User {
   _id?:   string;
   email:  string;
+  token:  string;
   name:   string;
   img?:   string;
   role:   IUserRole;
