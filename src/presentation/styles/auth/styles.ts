baea90cbd105f9efd7ddab88a9309506a33ac19b
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-07-23 00:32:47 -0500

    Remove unused style rules

--- src/presentation/styles/auth/styles.ts
+++ src/presentation/styles/auth/styles.ts
@@ -1,16 +1,9 @@
 import { StyleSheet } from 'react-native';
-import { globalStyles } from '../global.styles';
 
 export const authStyles = StyleSheet.create({
   formContainer: {
     gap: 10,
     marginTop: 20
-  },
-  footer: {
-    alignItems: 'flex-end',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    ...globalStyles.mainBackground
   },
   imageContainer: {
     alignItems: 'center',
@@ -19,9 +12,5 @@ export const authStyles = StyleSheet.create({
   mainImage: {
     height: 200,
     width: 300
-  },
-  spacer: {
-    ...globalStyles.mainBackground,
-    height: 20
   }
 });
