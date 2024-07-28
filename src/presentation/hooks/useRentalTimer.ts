eef0f8a61d0634aea74a783b6fa1d0d7a3d2497b
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-07-28 10:11:05 -0500

    Rentals timer improved: timer saved in storage before app is restarted and always incremental

--- src/presentation/hooks/useRentalTimer.ts
+++ src/presentation/hooks/useRentalTimer.ts
@@ -1,13 +1,15 @@
 import { useState, useEffect } from 'react';
 import { useTimer } from 'use-timer';
+import { StorageAdapter } from '../../config/adapters/storage-adapter';
 import { Rental } from '../../core/entities';
 
 export const useRentalTimer = ({ rental }: { rental: Rental; }) => {
+  const index = `rental_time_${rental.client}_${rental.vehicle.nickname}_${rental.date}`;
+  const [initialTime, setInitialTime] = useState<number | null>(null);
   const [done, setDone] = useState<boolean>(false);
-  const [timerType, setTimerType] = useState<'DECREMENTAL' | 'INCREMENTAL'>('DECREMENTAL');
   const { status, time, advanceTime, start, pause, reset } = useTimer({
-    initialTime: rental.time * 60,
-    timerType: timerType
+    initialTime: 0,
+    autostart: false
   });
   const buttonSize = { height: 30, width: 30 };
   const buttonOpacity = 0.7;
@@ -18,16 +20,45 @@ export const useRentalTimer = ({ rental }: { rental: Rental; }) => {
     return `${mins}:${secs < 10 ? '0' : ''}${secs}`;
   };
 
+  const saveTime = async (currentTime: number) => {
+    try {
+      await StorageAdapter.setItem(index, currentTime.toString());
+    } catch (e) {
+      console.error("Error saving time: ", e);
+    }
+  };
+
+  const loadTime = async () => {
+    try {
+      const savedTime = await StorageAdapter.getItem(index);
+      if (savedTime !== null) {
+        setInitialTime(parseInt(savedTime));
+      }
+    } catch (e) {
+      console.error("Error loading time: ", e);
+    }
+  };
+
   useEffect(() => {
-    if (!done) start();
-  }, [done]);
+    loadTime();
+  }, []);
+
+  useEffect(() => {
+    if (initialTime !== null) {
+      advanceTime(initialTime);
+      start();
+    }
+  }, [initialTime, advanceTime, start]);
 
   useEffect(() => {
     if (!done && time === 0) {
-      setTimerType('INCREMENTAL');
       start();
     }
-  }, [done, time]);
+  }, [done, time, start]);
+
+  useEffect(() => {
+    saveTime(time);
+  }, [time]);
 
   return {
     buttonOpacity,
