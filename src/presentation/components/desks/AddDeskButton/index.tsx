e805639991acd8da43d1fa5fd4c3ce3aaac95854
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-08-11 23:12:57 -0500

    Remove unnecessary props

--- src/presentation/components/desks/AddDeskButton/index.tsx
+++ src/presentation/components/desks/AddDeskButton/index.tsx
@@ -4,10 +4,10 @@ import { FAB } from '../../ui';
 import { DeskEntryModal } from '../DeskEntryModal';
 import { styles } from './styles';
 
-export const AddDeskButton = ({ top }: { top: number }) => {
+export const AddDeskButton = () => {
   const [visible, setVisible] = useState(false);
   return (
-    <Layout style={{ marginTop: top, ...styles.container }}>
+    <Layout style={styles.container}>
       <FAB iconName='plus-circle-outline' iconSize={50} onPress={() => setVisible(true)} />
       <DeskEntryModal visible={visible} setVisible={setVisible} />
     </Layout>
