f15f3235d90a73d2a234ae8bec0df2657cdd9f14
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-08-12 12:14:23 -0500

    Set image as button to open modal

--- src/presentation/components/profile/ProfileUserImage/index.tsx
+++ src/presentation/components/profile/ProfileUserImage/index.tsx
@@ -1,10 +1,9 @@
 import { useState } from 'react';
-import { Image } from 'react-native';
+import { Image, TouchableOpacity } from 'react-native';
 import { Layout } from '@ui-kitten/components';
 
-import { ProfileEditUserImageButton } from '../ProfileEditUserImageButton';
-import { ProfileUserImageModal } from '../ProfileUserImageModal';
 import { useCustomTheme, useUserInfo } from '../../../hooks';
+import { ProfileUserImageModal } from '../ProfileUserImageModal';
 
 import { globalColors } from '../../../theme/globalColors';
 import { styles } from './styles';
@@ -16,10 +15,9 @@ export const ProfileUserImage = () => {
 
   return (
     <Layout style={[styles.imageContainer, { backgroundColor: isDarkMode ? defaultBackgroundColor : globalColors.background }]}>
-      <Image style={styles.image} source={user?.img ? { uri: user.img } : require('../../../../assets/logo2.png')} />
-      <Layout style={styles.editImageIconContainer}>
-        <ProfileEditUserImageButton onPress={() => setVisible(true)} />
-      </Layout>
+      <TouchableOpacity activeOpacity={0.9} onPress={() => setVisible(true)}>
+        <Image style={styles.image} source={user?.img ? { uri: user.img } : require('../../../../assets/logo2.png')} />
+      </TouchableOpacity>
       <ProfileUserImageModal visible={visible} setVisible={setVisible} />
     </Layout>
   );
