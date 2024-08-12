243b3ee6cd4a6684f83dc452049f45d10554abad
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-08-11 23:25:53 -0500

    Update component style and conditional rendering

--- src/presentation/components/desks/DesksDataContainer/index.tsx
+++ src/presentation/components/desks/DesksDataContainer/index.tsx
@@ -1,14 +1,15 @@
 import { useEffect } from 'react';
-import { useWindowDimensions } from 'react-native';
+import { Platform } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { DataLayout, CustomDivider, TopNavigation } from '../../ui';
-import { DeskListComponent } from '../DeskListComponent';
-import { LoadingScreen } from '../../../screens/LoadingScreen';
+import { Layout } from '@ui-kitten/components';
+import { useCustomTheme } from '../../../hooks';
 import { useDesksStore } from '../../../store/desk/useDeskStore';
+import { DataLayout, CustomDivider, TopNavigation, EmptyListMessage } from '../../ui';
+import { DeskListComponent } from '../DeskListComponent';
 
 export const DesksDataContainer = () => {
+  const { background } = useCustomTheme();
   const { bottom, top } = useSafeAreaInsets();
-  const { height } = useWindowDimensions();
   const desks = useDesksStore(state => state.desks);
   const getAllDesks = useDesksStore(state => state.getAllDesks);
   const totalDesks = desks.length === 0;
@@ -18,13 +19,17 @@ export const DesksDataContainer = () => {
   }, []);
 
   return (
-    <DataLayout paddingTop={height * 0.042}>
+    <DataLayout paddingTop={Platform.OS === 'ios' ? top : top + 20}>
       <TopNavigation top={top} title='Puestos de trabajo' />
       <CustomDivider />
-      {totalDesks
-        ? <LoadingScreen />
-        : <DeskListComponent bottom={bottom} desks={desks} />
-      }
+      <Layout style={[{ paddingBottom: bottom + 50 }, background]}>
+        {totalDesks
+          ?
+          <EmptyListMessage heightBy={0.9} text='No hay puestos de trabajo' />
+          :
+          <DeskListComponent bottom={bottom} desks={desks} />
+        }
+      </Layout>
     </DataLayout>
   );
 };
