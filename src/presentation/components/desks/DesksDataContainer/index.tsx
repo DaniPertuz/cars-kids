aa12942780477207cd3e3c87936c8a423d36b867
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-08-25 00:39:55 -0500

    Remove data coming from store

--- src/presentation/components/desks/DesksDataContainer/index.tsx
+++ src/presentation/components/desks/DesksDataContainer/index.tsx
@@ -1,24 +1,16 @@
-import { useEffect } from 'react';
 import { Platform } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { useDesksStore } from '../../../store/desk/useDeskStore';
 import { DataLayout, CustomDivider, TopNavigation } from '../../ui';
 import { DeskListComponent } from '../DeskListComponent';
 
 export const DesksDataContainer = () => {
   const { top } = useSafeAreaInsets();
-  const desks = useDesksStore(state => state.desks);
-  const getAllDesks = useDesksStore(state => state.getAllDesks);
-
-  useEffect(() => {
-    getAllDesks();
-  }, []);
 
   return (
     <DataLayout paddingTop={Platform.OS === 'ios' ? top : top + 20}>
       <TopNavigation top={top} title='Puestos de trabajo' />
       <CustomDivider />
-      <DeskListComponent desks={desks} />
+      <DeskListComponent />
     </DataLayout>
   );
 };
