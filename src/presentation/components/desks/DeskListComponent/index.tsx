f1199a8b3032dc3cf6396540942a8d77708df2ba
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-08-25 00:38:45 -0500

    Improve conditional rendering while loading data and update styles

--- src/presentation/components/desks/DeskListComponent/index.tsx
+++ src/presentation/components/desks/DeskListComponent/index.tsx
@@ -1,18 +1,41 @@
+import { useEffect, useState } from 'react';
 import { Layout } from '@ui-kitten/components';
 import { Desk } from '../../../../core/entities';
+import * as DesksUseCases from '../../../../core/use-cases/desks';
 import { useCustomTheme } from '../../../hooks';
+import { LoadingScreen } from '../../../screens/LoadingScreen';
 import { EmptyListMessage } from '../../ui';
 import { DeskList } from '../DeskList';
 
-interface Props {
-  desks: Desk[];
-}
-
-export const DeskListComponent = ({ desks }: Props) => {
+export const DeskListComponent = () => {
   const { background } = useCustomTheme();
+  const [desks, setDesks] = useState<Desk[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = async () => {
+    const desks = await DesksUseCases.getDesksUseCase('desks');
+    const resp = desks.response;
+    if (!resp || !resp.desks) {
+      setDesks([]);
+      setLoading(false);
+      return;
+    }
+
+    setDesks(resp.desks);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <Layout style={[{ marginHorizontal: 20, height: '90%', marginBottom: 15 }, background]}>
-      {(desks.length === 0)
+    <Layout style={[{ marginHorizontal: 20, height: '85%', marginBottom: '25%', paddingBottom: '15%' }, background]}>
+      {loading
+        ?
+        <LoadingScreen />
+        :
+        (desks.length === 0)
           ?
           <EmptyListMessage heightBy={0.9} text='No hay puestos de trabajo' />
           :
