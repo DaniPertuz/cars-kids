718cb208fc93c7c53e556ccc47274273cd799d4c
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-09-16 01:52:37 -0500

    Update styles rules

--- src/presentation/components/ui/TotalListMessage/styles.tsx
+++ src/presentation/components/ui/TotalListMessage/styles.tsx
@@ -1,10 +1,8 @@
 import { StyleSheet } from 'react-native';
-import { globalStyles } from '../../../styles/global.styles';
 
 export const styles = StyleSheet.create({
   container: {
     left: 25,
-    position: 'absolute',
-    ...globalStyles.mainBackground
+    position: 'absolute'
   }
 });
