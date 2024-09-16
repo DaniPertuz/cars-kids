8667f94a1c2061d8bc096e11d71599a9b001f0be
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-09-15 23:54:25 -0500

    Update background and custom components implementation

--- src/presentation/components/users/UserEntryModal/index.tsx
+++ src/presentation/components/users/UserEntryModal/index.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 import { Card, Layout, Modal } from '@ui-kitten/components';
-
 import { SnackbarAdapter } from '../../../../config/adapters/snackbar.adapter';
 import { User } from '../../../../core/entities';
 import * as UserUseCases from '../../../../core/use-cases/users';
 import { IUserRole } from '../../../../infrastructure/interfaces';
-import { RadioGroupComponent, PrimaryButton, HeaderFive, Callout } from '../../ui';
-
+import { useCustomTheme } from '../../../hooks';
+import { RadioGroupComponent, PrimaryButton, Callout } from '../../ui';
+import { UserEntryModalTitle } from './UserEntryModalTitle';
 import { globalStyles } from '../../../styles/global.styles';
 
 interface Props {
@@ -16,6 +16,7 @@ interface Props {
 }
 
 export const UserEntryModal = ({ user, visible, setVisible }: Props) => {
+  const { background } = useCustomTheme();
   const [loading, setLoading] = useState(false);
   const [userRole, setUserRole] = useState(IUserRole.Editor);
   const initialUserIndex = user.role === IUserRole.Admin ? 0 : 1;
@@ -51,9 +52,9 @@ export const UserEntryModal = ({ user, visible, setVisible }: Props) => {
 
   return (
     <Modal visible={visible} backdropStyle={globalStyles.backdrop} onBackdropPress={() => setVisible(false)}>
-      <Card style={globalStyles.mainBackground}>
+      <Card style={background}>
+        <UserEntryModalTitle title='Actualizar rol de usuario' onPress={() => setVisible(false)} />
         <Layout style={globalStyles.modalContainer}>
-          <HeaderFive text={'Actualizar rol de usuario'} />
           <Callout text={user.email} />
           <RadioGroupComponent initialValue={initialUserIndex} list={[IUserRole.Admin, IUserRole.Editor]} handleSelection={handleUserRole} />
           <PrimaryButton disabled={loading} text={'Actualizar'} onPress={onUpdate} />
