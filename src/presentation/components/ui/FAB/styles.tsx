d6d887f07ec7c8047ee3b5433dde64ace74adaf5
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-09-16 01:40:00 -0500

    Update style rules

--- src/presentation/components/ui/FAB/styles.tsx
+++ src/presentation/components/ui/FAB/styles.tsx
@@ -1,10 +1,10 @@
-import { Platform, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
     borderRadius: 100,
     position: 'absolute',
     right: 20,
-    bottom: Platform.OS === 'ios' ? 115 : 80
+    bottom: 0
   }
 });
