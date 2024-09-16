a2644af7e17de9dd0915dabf4033b8f436fd2da4
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-09-16 01:42:31 -0500

    Update custom UI AddButton component

--- src/presentation/components/ui/AddButton/index.tsx
+++ src/presentation/components/ui/AddButton/index.tsx
@@ -1,19 +1,29 @@
-import { TouchableOpacity } from 'react-native';
-import { Icon } from '@ui-kitten/components';
+import { useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { Layout } from '@ui-kitten/components';
+import { useCustomTheme } from '../../../hooks';
+import { Product } from '../../../../core/entities/product';
+import { Vehicle } from '../../../../core/entities';
+import { FAB } from '../../ui';
 
 interface Props {
-  iconSize: number;
-  onPress: () => void;
+  Modal: (props: { entity?: Product | Vehicle; visible: boolean; setVisible: (value: boolean) => void; }) => React.JSX.Element;
 }
 
-export const AddButton = ({ iconSize, onPress }: Props) => {
+export const AddButton = ({ Modal }: Props) => {
+  const [visible, setVisible] = useState(false);
+  const { background } = useCustomTheme();
   return (
-    <TouchableOpacity
-      activeOpacity={1.0}
-      onPress={onPress}
-      style={{ height: iconSize, width: iconSize }}
-    >
-      <Icon name='plus-circle-outline' />
-    </TouchableOpacity>
+    <Layout style={[styles.container, background]}>
+      <FAB iconName='plus-circle-outline' iconSize={50} onPress={() => setVisible(true)} />
+      <Modal visible={visible} setVisible={setVisible} />
+    </Layout>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 100,
+    borderColor: 'transparent'
+  }
+});
