20088d39d99b44190b069a6608afa3b08c63aa04
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-09-19 00:32:46 -0500

    Update screen content

--- src/presentation/screens/LoadingScreen.tsx
+++ src/presentation/screens/LoadingScreen.tsx
@@ -1,12 +1,40 @@
-import { Layout, Spinner } from '@ui-kitten/components';
+import { useEffect, useRef } from 'react';
+import { Animated } from 'react-native';
+import { Layout } from '@ui-kitten/components';
 import { useCustomTheme } from '../hooks';
 import { globalStyles } from '../styles/global.styles';
 
 export const LoadingScreen = () => {
   const { background } = useCustomTheme();
+  const scaleAnim = useRef(new Animated.Value(1)).current;
+
+  useEffect(() => {
+    const animateScale = Animated.loop(
+      Animated.sequence([
+        Animated.timing(scaleAnim, {
+          toValue: 1.2,
+          duration: 300,
+          useNativeDriver: true
+        }),
+        Animated.timing(scaleAnim, {
+          toValue: 1.0,
+          duration: 300,
+          useNativeDriver: true
+        }),
+      ])
+    );
+
+    animateScale.start();
+
+    return () => animateScale.stop();
+  }, [scaleAnim]);
+
   return (
     <Layout style={[globalStyles.flexAlignJustifyCenter, background]}>
-      <Spinner size='giant' style={globalStyles.redBorder} />
+      <Animated.Image
+        source={require('../../assets/carkids-removebg.png')}
+        style={[{ height: 80, width: 150 }, background, { transform: [{ scale: scaleAnim }] }]}
+      />
     </Layout>
   );
 };
