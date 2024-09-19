27fbcd748979fd2e9c4766030f039bef1c6d2e60
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-09-18 22:53:06 -0500

    Implement custom Vehicle item components

--- src/presentation/components/vehicles/VehicleDescription/index.tsx
+++ src/presentation/components/vehicles/VehicleDescription/index.tsx
@@ -1,11 +1,12 @@
 import { Platform } from 'react-native';
 import { Layout } from '@ui-kitten/components';
-import { IStatus, IUserRole } from '../../../../infrastructure/interfaces';
-import { useCustomTheme } from '../../../hooks';
-import { useAuthStore } from '../../../store/auth/useAuthStore';
 import { Vehicle } from '../../../../core/entities';
-import { CalloutBold, Caption } from '../../ui';
-import { VehicleColor } from '../VehicleColor';
+import { IUserRole } from '../../../../infrastructure/interfaces';
+import { useAuthStore } from '../../../store/auth/useAuthStore';
+import { useCustomTheme } from '../../../hooks';
+import { VehicleDescriptionCategory } from './VehicleDescriptionCategory';
+import { VehicleDescriptionStatus } from './VehicleDescriptionStatus';
+import { VehicleDescriptionColor } from './VehicleDescriptionColor';
 import { styles } from './styles';
 
 interface Props {
@@ -17,10 +18,10 @@ export const VehicleDescription = ({ vehicle }: Props) => {
   const { user } = useAuthStore();
   const isAdmin = user?.role === IUserRole.Admin;
   return (
-    <Layout style={[{ ...styles.descriptionContainer, width: isAdmin ? Platform.OS === 'ios' ? '55%' : '65%' : Platform.OS === 'ios' ? '35%' : '45%' }, platinumItemBackgroundColor]}>
-      <Caption text={(vehicle.category === 'car' ? 'Carro' : 'Moto')} />
-      {user?.role === IUserRole.Admin && <CalloutBold text={(vehicle.status === IStatus.Active ? 'Activo' : 'Inactivo')} />}
-      <VehicleColor vehicle={vehicle} />
+    <Layout style={[{ ...styles.descriptionContainer, width: isAdmin ? Platform.OS === 'ios' ? '70%' : '80%' : Platform.OS === 'ios' ? '50%' : '60%' }, platinumItemBackgroundColor]}>
+      <VehicleDescriptionCategory vehicle={vehicle} />
+      <VehicleDescriptionStatus vehicle={vehicle} />
+      <VehicleDescriptionColor vehicle={vehicle} />
     </Layout>
   );
 };
