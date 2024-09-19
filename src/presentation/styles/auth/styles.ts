6af5532cac7309eb636d2548d64a2cd87bd4fb84
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-09-19 00:07:21 -0500

    Update style rules

--- src/presentation/styles/auth/styles.ts
+++ src/presentation/styles/auth/styles.ts
@@ -2,12 +2,11 @@ import { StyleSheet } from 'react-native';
 
 export const authStyles = StyleSheet.create({
   formContainer: {
-    gap: 10,
+    gap: 15,
     marginTop: 20
   },
   imageContainer: {
-    alignItems: 'center',
-    marginBottom: 30
+    alignItems: 'center'
   },
   mainImage: {
     height: 200,
