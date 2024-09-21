060fe782cfa5f2d7dd45389b1abfd4cd7fa1cf9b
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-09-21 00:55:24 -0500

    Update component structure and styles

--- src/presentation/components/transactions/TransactionEntryModalPaymentFeesSelects/index.tsx
+++ src/presentation/components/transactions/TransactionEntryModalPaymentFeesSelects/index.tsx
@@ -1,3 +1,4 @@
+import { StyleSheet } from 'react-native';
 import { Layout } from '@ui-kitten/components';
 import { Fee } from '../../../../infrastructure/interfaces';
 import { paymentDescriptions } from '../../../../utils';
@@ -18,20 +19,35 @@ interface Props {
 
 const FeeSelector = ({ fee, handleFeePayment, handleFeeValue, setFee }: { fee: Fee; handleFeePayment: Function; handleFeeValue: Function; setFee: Function; }) => {
   return (
-    <>
-      <SelectComponent placeholder={'Medio de pago'} initialValue='' options={Object.values(paymentDescriptions)} handleSelection={(value: string) => handleFeePayment(setFee, value)} />
-      <NumericInput placeholder={'Valor'} onChangeText={(value: number) => handleFeeValue(setFee, Number(value))} value={fee.price} caption={'Cuota'} />
-    </>
+    <Layout style={styles.selectorContainer}>
+      <SelectComponent style={styles.flexOne} placeholder={'Medio'} initialValue='' options={Object.values(paymentDescriptions)} handleSelection={(value: string) => handleFeePayment(setFee, value)} />
+      <NumericInput style={styles.flexOne} placeholder={'Valor'} onChangeText={(value: number) => handleFeeValue(setFee, Number(value))} value={fee.price} caption={'Cuota'} />
+    </Layout>
   );
 };
 
 export const TransactionEntryModalPaymentFeesSelects = ({ firstFee, secondFee, thirdFee, handleFeePayment, handleFeeValue, setFirstFee, setSecondFee, setThirdFee }: Props) => {
   const { background } = useCustomTheme();
   return (
-    <Layout style={[{ gap: 10 }, background]}>
+    <Layout style={[styles.feeSelectContainer, background]}>
       <FeeSelector fee={firstFee} handleFeePayment={handleFeePayment} handleFeeValue={handleFeeValue} setFee={setFirstFee} />
       <FeeSelector fee={secondFee} handleFeePayment={handleFeePayment} handleFeeValue={handleFeeValue} setFee={setSecondFee} />
       <FeeSelector fee={thirdFee} handleFeePayment={handleFeePayment} handleFeeValue={handleFeeValue} setFee={setThirdFee} />
     </Layout>
   );
 };
+
+const styles = StyleSheet.create({
+  flexOne: {
+    flex: 1
+  },
+  selectorContainer: {
+    flexDirection: 'row',
+    gap: 10,
+    justifyContent: 'space-between'
+  },
+  feeSelectContainer: {
+    gap: 10,
+    marginTop: 20
+  }
+});
