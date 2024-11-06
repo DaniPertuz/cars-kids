b7b613d2513b6655dac5ea493a8b93dd201fff13
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-11-05 23:00:22 -0500

    Update vehicle data handling

--- src/presentation/components/ui/EditButton/index.tsx
+++ src/presentation/components/ui/EditButton/index.tsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
 import { TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
 import { Layout } from '@ui-kitten/components';
 
+import { RootStackParams } from '../../../navigation/MainNavigator';
 import { Budget, Desk, Product, Purchase, Rental, User, Vehicle } from '../../../../core/entities';
 import { BudgetEntryModal } from '../../budget/BudgetEntryModal';
 import { DeskEntryModal } from '../../desks/DeskEntryModal';
@@ -28,9 +31,19 @@ interface Props {
 
 export const EditButton = ({ iconSize, budget, desk, updateDesk, product, purchase, rental, user, vehicle }: Props) => {
   const [visible, setVisible] = useState(false);
+  const navigation = useNavigation<StackNavigationProp<RootStackParams>>();
+
+  const handlePress = () => {
+    if (vehicle) {
+      navigation.navigate('VehicleDetailsScreen', { vehicle });
+      return;
+    }
+
+    setVisible(true);
+  };
 
   return (
-    <TouchableOpacity activeOpacity={0.7} onPress={() => setVisible(true)}>
+    <TouchableOpacity activeOpacity={0.7} onPress={handlePress}>
       <Layout style={styles.container}>
         <CustomIcon name='edit-outline' size={{ height: iconSize, width: iconSize }} fillColor={globalColors.white} />
         {budget && <BudgetEntryModal budget={budget} visible={visible} setVisible={setVisible} />}
@@ -39,7 +52,7 @@ export const EditButton = ({ iconSize, budget, desk, updateDesk, product, purcha
         {purchase && <TransactionEntryModal visible={visible} setVisible={setVisible} purchase={purchase} desk={desk!} transaction={'Purchase'} />}
         {rental && <TransactionEntryModal visible={visible} setVisible={setVisible} rental={rental} desk={desk!} transaction={'Rental'} />}
         {user && <UserEntryModal user={user} visible={visible} setVisible={setVisible} />}
-        {vehicle && <VehicleEntryModal vehicle={vehicle} visible={visible} setVisible={setVisible} />}
+        {vehicle && <VehicleEntryModal visible={visible} setVisible={setVisible} />}
       </Layout>
     </TouchableOpacity>
   );
