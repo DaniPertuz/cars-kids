5fa4749576070b00b453cf9d9633b1a969c7efb5
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-11-05 20:28:46 -0500

    Add autoFocus property handling

--- src/presentation/components/forms/default-input/index.tsx
+++ src/presentation/components/forms/default-input/index.tsx
@@ -2,18 +2,20 @@ import { Input } from '@ui-kitten/components';
 import { useCustomTheme } from '../../../hooks';
 
 interface Props {
+  autoFocus?: boolean;
   caption?: string;
   placeholder: string;
   value: string;
   onChangeText: (text: string) => void;
 }
 
-export const DefaultInput = ({ caption, placeholder, value, onChangeText }: Props) => {
+export const DefaultInput = ({ autoFocus, caption, placeholder, value, onChangeText }: Props) => {
   const { borderColor } = useCustomTheme();
 
   return (
     <Input
       placeholder={placeholder}
+      autoFocus={autoFocus}
       label={placeholder}
       caption={caption}
       keyboardType='default'
